e741dd9e6c87ae57c52b0ab8c7b8fc72b94e2618
Cheng Wang <wachmc@gmail.com>
2018-03-23 20:57:41 +0100

    Initial commit

---
+++ project/Dependencies.scala
@@ -0,0 +1,16 @@
+import sbt._
+
+object Version {
+  lazy val akka = "2.5.11"
+}
+
+object Dependencies {
+  lazy val scalaTest       = "org.scalatest"              %% "scalatest"      % "3.0.3"
+  lazy val cats            = "org.typelevel"              %% "cats"           % "0.9.0"
+  lazy val akka            = "com.typesafe.akka"          %% "akka-actor"     % Version.akka
+  lazy val `akka-slf4j`    = "com.typesafe.akka"          %% "akka-slf4j"     % Version.akka
+  lazy val akkTest         = "com.typesafe.akka"          %% "akka-testkit"   % Version.akka % Test
+  lazy val scrypto         = "org.scorexfoundation"       %% "scrypto"        % "2.0.0"
+  lazy val `scala-logging` = "com.typesafe.scala-logging" %% "scala-logging"  % "3.8.0"
+  lazy val logback         = "ch.qos.logback"             % "logback-classic" % "1.2.3"
+}
