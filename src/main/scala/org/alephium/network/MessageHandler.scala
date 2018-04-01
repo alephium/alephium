14be7a473341e6f41d46cd2f207e38fb49f89edf
Cheng Wang <wachmc@gmail.com>
2018-04-01 19:00:50 +0200

    Fix postfix operator

--- src/main/scala/org/alephium/network/MessageHandler.scala
+++ src/main/scala/org/alephium/network/MessageHandler.scala
@@ -7,6 +7,7 @@ import org.alephium.util.BaseActor
 
 import scala.util.Random
 import scala.concurrent.duration._
+import scala.language.postfixOps
 
 object MessageHandler {
   sealed trait Command
