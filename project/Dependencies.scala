93850d66486fdf0ed1540cd792ccf9e66d23ec3d
Cheng Wang <wachmc@gmail.com>
2018-04-17 15:54:14 +0200

    Add akka test kit

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val scalaTest       = "org.scalatest"              %% "scalatest"      % "3.0.5"
   lazy val akka            = "com.typesafe.akka"          %% "akka-actor"     % Version.akka
   lazy val `akka-slf4j`    = "com.typesafe.akka"          %% "akka-slf4j"     % Version.akka
-  lazy val akkTest         = "com.typesafe.akka"          %% "akka-testkit"   % Version.akka % Test
+  lazy val akkaTest        = "com.typesafe.akka"          %% "akka-testkit"   % Version.akka % Test
   lazy val bcprov          = "org.bouncycastle"           % "bcprov-jdk15on"  % "1.59"
   lazy val `scala-logging` = "com.typesafe.scala-logging" %% "scala-logging"  % "3.9.0"
   lazy val logback         = "ch.qos.logback"             % "logback-classic" % "1.2.3"
