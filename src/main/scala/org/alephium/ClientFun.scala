c3d697b429b651006dd853d8b13935acdf869032
Cheng Wang <wachmc@gmail.com>
2018-06-23 17:09:03 +0200

    Use P2P network

--- src/main/scala/org/alephium/ClientFun.scala
+++ src/main/scala/org/alephium/ClientFun.scala
@@ -1,7 +1,5 @@
 package org.alephium
 
-import java.net.InetSocketAddress
-
 import org.alephium.client.{Miner, Node}
 import org.alephium.constant.Network
 import org.alephium.crypto.ED25519PublicKey
@@ -11,11 +9,12 @@ import org.alephium.util.Hex._
 
 // scalastyle:off magic.number
 object ClientFun extends App {
-  val node = Node("ClientFun", 10304)
+  val node = Node("ClientFun", Network.port)
   import node.{blockHandler, peerManager, system}
 
-  val remote = new InetSocketAddress(Network.port)
-  peerManager ! PeerManager.Connect(remote)
+  Network.peers.foreach { remote =>
+    peerManager ! PeerManager.Connect(remote)
+  }
 
   val publicKey: ED25519PublicKey = ED25519PublicKey.unsafeFrom(
     hex"2db399c90fee96ec2310b62e3f62b5bd87972a96e5fa64675f0adc683546cd1d")
@@ -23,9 +22,11 @@ object ClientFun extends App {
 
   miner ! Miner.Start
 
-  Thread.sleep(1000)
-  0 to 5 foreach { _ =>
-    Thread.sleep(3 * 1000)
-    blockHandler.tell(BlockHandler.PrepareSync(remote), peerManager)
+  if (Network.peers.nonEmpty) {
+    Thread.sleep(1000)
+    0 to 5 foreach { _ =>
+      Thread.sleep(3 * 1000)
+      blockHandler.tell(BlockHandler.PrepareSync(Network.peers.head), peerManager)
+    }
   }
 }
