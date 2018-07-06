b06b3655b32e01f87c749cb0978fdde9251b38d6
Cheng Wang <chengwang0511@gmail.com>
2018-07-06 12:11:54 +0200

    Small fixes

--- src/main/scala/org/alephium/storage/BlockHandler.scala
+++ src/main/scala/org/alephium/storage/BlockHandler.scala
@@ -50,19 +50,18 @@ class BlockHandler() extends BaseActor {
 
   def handleWith(peerManager: ActorRef): Receive = {
     case AddBlocks(blocks) =>
-      if (blocks.nonEmpty) {
-        val ok = blockFlow.addBlocks(blocks)
-        if (ok) {
-          val length = blockFlow.getBestLength
-          val info   = blockFlow.getInfo
-          log.debug(s"Add ${blocks.size} blocks, #length: $length, info: $info")
-          if (blocks.size == 1) {
-            log.debug(s"Got new block for ${blockFlow.getIndex(blocks.head)}, broadcast it")
-            peerManager ! PeerManager.BroadCast(Message(SendBlocks(blocks)), sender())
-          }
-        } else {
-          log.warning(s"Failed to add a new block")
+      require(blocks.nonEmpty)
+      val ok = blockFlow.addBlocks(blocks)
+      if (ok) {
+        val length = blockFlow.getBestLength
+        val info   = blockFlow.getInfo
+        log.debug(s"Add ${blocks.size} blocks, #length: $length, info: $info")
+        if (blocks.size == 1) {
+          log.debug(s"Got new block for ${blockFlow.getIndex(blocks.head)}, broadcast it")
+          peerManager ! PeerManager.BroadCast(Message(SendBlocks(blocks)), sender())
         }
+      } else {
+        log.warning(s"Failed to add a new block")
       }
     case GetBlocksAfter(locators) =>
       val newBlocks = blockFlow.getBlocks(locators)
