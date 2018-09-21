853a5796d9b033d55b5238e9e5b40d5479ce13c5
Cheng Wang <wachmc@gmail.com>
2018-09-21 15:46:34 +0200

    Renaming and move things around

--- shared/src/main/scala/org/alephium/storage/SingleChain.scala
+++ shared/src/main/scala/org/alephium/storage/SingleChain.scala
@@ -1,4 +1,5 @@
 package org.alephium.storage
+import org.alephium.crypto.Keccak256
 import org.alephium.protocol.model.Block
 
 trait SingleChain extends BlockPool {
@@ -6,4 +7,12 @@ trait SingleChain extends BlockPool {
   def maxHeight: Int
 
   def add(block: Block, weight: Int): Boolean
+}
+
+sealed trait AddBlockResult
+
+object AddBlockResult {
+  case object Success                          extends AddBlockResult
+  case object AlreadyExisted                   extends AddBlockResult
+  case class MissingDeps(deps: Seq[Keccak256]) extends AddBlockResult
 }
--- shared/src/test/scala/org/alephium/storage/FlowHandlerSpec.scala
+++ shared/src/test/scala/org/alephium/storage/FlowHandlerSpec.scala
@@ -2,7 +2,7 @@ package org.alephium.storage
 
 import org.alephium.{AlephiumActorSpec, TxFixture}
 
-class BlockHandlerSpec extends AlephiumActorSpec("block_handler_spec") with TxFixture {
+class FlowHandlerSpec extends AlephiumActorSpec("flow_handler_spec") with TxFixture {
 
   behavior of "BlockPoolHandler"
 }
