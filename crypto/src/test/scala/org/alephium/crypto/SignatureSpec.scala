358477fec0870a6974ec40348c7fd4ff8fee3474
Cheng Wang <wachmc@gmail.com>
2018-10-20 11:14:13 +0200

    Minor update

--- crypto/src/test/scala/org/alephium/crypto/SignatureSpec.scala
+++ crypto/src/test/scala/org/alephium/crypto/SignatureSpec.scala
@@ -3,8 +3,9 @@ package org.alephium.crypto
 import org.alephium.util.{AVector, AlephiumSpec}
 
 class SignatureSpec extends AlephiumSpec {
+
   "ED25519" should "sign correctly" in {
-    forAll { (_message: Seq[Byte]) =>
+    forAll { _message: IndexedSeq[Byte] =>
       val message  = AVector.from(_message)
       val (sk, pk) = ED25519.generateKeyPair()
       val sign     = ED25519.sign(message, sk)
@@ -13,9 +14,7 @@ class SignatureSpec extends AlephiumSpec {
   }
 
   it should "be verified with proper public key" in {
-    forAll { (_message1: Seq[Byte], _message2: Seq[Byte]) =>
-      val message1 = AVector.from(_message1)
-      val message2 = AVector.from(_message2)
+    forAll(bytesGen, bytesGen) { (message1: AVector[Byte], message2: AVector[Byte]) =>
       whenever(message1 != message2) {
         val (sk1, pk1) = ED25519.generateKeyPair()
         val (_, pk2)   = ED25519.generateKeyPair()
