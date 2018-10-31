e8c993c77dba0a2dfec93e97e3d66b87eb30d5c8
Alois Cochard <alois.cochard@gmail.com>
2018-10-31 16:48:00 +0100

    Fix bit-shifting usage in `ConfigFixture`.

--- protocol/src/test/scala/org/alephium/protocol/model/ConfigFixture.scala
+++ protocol/src/test/scala/org/alephium/protocol/model/ConfigFixture.scala
@@ -10,7 +10,7 @@ trait ConfigFixture {
     override val groups: Int = 3
 
     override def numZerosAtLeastInHash: Int = 0
-    override def maxMiningTarget: BigInt    = BigInt(1) << 256 - 1
+    override def maxMiningTarget: BigInt    = (BigInt(1) << 256) - 1
     override def blockTargetTime: Duration  = Duration.ofMinutes(4)
     override def blockConfirmNum: Int       = 2
     override def retargetInterval: Int      = 180
