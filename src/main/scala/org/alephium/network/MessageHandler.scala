ab874bb2ed19098f9544e5881ffc323c42d4835e
Cheng Wang <wachmc@gmail.com>
2018-12-02 21:06:34 +0100

    Send ping message periodically

--- src/main/scala/org/alephium/network/MessageHandler.scala
+++ src/main/scala/org/alephium/network/MessageHandler.scala
@@ -1,26 +1,38 @@
 package org.alephium.network
 
-import akka.actor.ActorRef
+import akka.actor.{ActorRef, Timers}
 import akka.io.Tcp
 import org.alephium.network.message.{NetworkMessage, Ping, Pong}
 import org.alephium.util.BaseActor
 
 import scala.util.Random
+import scala.concurrent.duration._
 
-trait MessageHandler extends BaseActor {
+object MessageHandler {
+  sealed trait Command
+  case object SendPing extends Command
+}
+
+trait MessageHandler extends BaseActor with Timers {
 
   def envelope(message: NetworkMessage): Tcp.Write =
     Tcp.Write(NetworkMessage.serializer.serialize(message))
 
-  def handleMessage(message: NetworkMessage): Unit = {
+  def forMessageHandler(connection: ActorRef): Receive = {
+    case MessageHandler.SendPing =>
+      sendPing(connection)
+  }
+
+  def handleMessage(message: NetworkMessage, connection: ActorRef): Unit = {
     val payload = message.payload
     payload match {
       case Ping(nonce) =>
         logger.debug("Ping received, response with pong")
-        self ! NetworkMessage(Pong(nonce))
+        connection ! envelope(NetworkMessage(Pong(nonce)))
       case Pong(nonce) =>
         if (nonce == pingNonce) {
           logger.debug("Pong received, no response")
+          pingNonce = 0
         } else {
           logger.debug(s"Pong received with wrong nonce: expect $pingNonce, got $nonce")
           context stop self
@@ -31,8 +43,14 @@ trait MessageHandler extends BaseActor {
   // nonce for ping/pong message
   private var pingNonce: Int = 0
 
-  def startPing(connection: ActorRef): Unit = {
-    pingNonce = Random.nextInt()
-    connection ! envelope(NetworkMessage(Ping(pingNonce)))
+  def sendPing(connection: ActorRef): Unit = {
+    if (pingNonce != 0) {
+      logger.debug("No pong message received in time")
+      context stop self
+    } else {
+      pingNonce = Random.nextInt()
+      connection ! envelope(NetworkMessage(Ping(pingNonce)))
+      timers.startSingleTimer(MessageHandler, MessageHandler.SendPing, 1 minute)
+    }
   }
 }
--- src/main/scala/org/alephium/network/TcpHandler.scala
+++ src/main/scala/org/alephium/network/TcpHandler.scala
@@ -17,18 +17,18 @@ trait TcpHandler extends MessageHandler {
 
   def awaitStart(connection: ActorRef): Receive = {
     case TcpHandler.Start =>
-      startPing(connection)
       context.become(handle(connection))
+      self ! MessageHandler.SendPing
   }
 
   def handle(connection: ActorRef): Receive =
-    handleEvent orElse handleCommand(connection)
+    handleEvent(connection) orElse handleCommand(connection) orElse forMessageHandler(connection)
 
-  def handleEvent: Receive = {
+  def handleEvent(connection: ActorRef): Receive = {
     case Tcp.Received(data) =>
       val message = NetworkMessage.deserializer.deserialize(data).get
       logger.debug(s"Received $message from $remote")
-      handleMessage(message)
+      handleMessage(message, connection)
     case closeEvent @ (Tcp.ConfirmedClosed | Tcp.Closed | Tcp.Aborted | Tcp.PeerClosed) =>
       logger.debug(s"Connection closed: $closeEvent")
       context stop self
