3dff619c9d918141ff75a07e5b777d22e3151067
Cheng Wang <wachmc@gmail.com>
2018-12-03 23:39:42 +0100

    Improve crypto

--- src/main/scala/org/alephium/crypto/Sha256.scala
+++ src/main/scala/org/alephium/crypto/Sha256.scala
@@ -8,12 +8,12 @@ case class Sha256(bytes: Seq[Byte]) extends HashOutput
 object Sha256 extends Hash[Sha256] {
   override val size: Int = 32
 
+  override val provider: Digest = new SHA256Digest()
+
   private def apply(digest: Seq[Byte]): Sha256 = {
     require(digest.length == size)
     new Sha256(digest)
   }
 
   override def unsafeFrom(digest: Seq[Byte]): Sha256 = apply(digest)
-
-  override val provider: Digest = new SHA256Digest()
 }
