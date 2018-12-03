f1a6a5a1e6aee2db32c74ad614bf3283ad23e5c0
Cheng Wang <wachmc@gmail.com>
2018-12-03 03:39:15 +0100

    Add simple data model

--- src/main/scala/org/alephium/primitive/BlockHeader.scala
+++ src/main/scala/org/alephium/primitive/BlockHeader.scala
@@ -2,4 +2,11 @@ package org.alephium.primitive
 
 import org.alephium.crypto._
 
-case class BlockHeader(hash: Sha256, nonce: Int)
+case class BlockHeader(
+    prevBlock: Sha256,
+    txsHash: Sha256,
+    timestamp: Int,
+    difficulty: Int,
+    transactionCnt: Int,
+    nonce: Int
+)
