ffe0d16db043c74f663624999c9a0e22f64bad1b
Cheng Wang <wachmc@gmail.com>
2018-12-03 21:49:17 +0100

    Improve crypto

--- src/main/scala/org/alephium/crypto/Keccak256.scala
+++ src/main/scala/org/alephium/crypto/Keccak256.scala
@@ -8,12 +8,12 @@ case class Keccak256(bytes: Seq[Byte]) extends HashOutput
 object Keccak256 extends Hash[Keccak256] {
   override val size: Int = 32
 
+  override val provider: Digest = new KeccakDigest(size * 8)
+
   private def apply(digest: Seq[Byte]): Keccak256 = {
     require(digest.length == size)
     new Keccak256(digest)
   }
 
   override def unsafeFrom(digest: Seq[Byte]): Keccak256 = apply(digest)
-
-  override val provider: Digest = new KeccakDigest(size * 8)
 }
