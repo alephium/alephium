dd1875ba8505f2aa596b892b29882e384a84aa87
Cheng Wang <wachmc@gmail.com>
2018-12-05 15:22:50 +0100

    Read genesis from json file

--- src/main/scala/org/alephium/constant/Protocol.scala
+++ src/main/scala/org/alephium/constant/Protocol.scala
@@ -1,23 +1,5 @@
 package org.alephium.constant
 
-import org.alephium.crypto.{ED25519PrivateKey, ED25519PublicKey, ED25519Signature}
-import org.alephium.protocol.model._
-import org.alephium.util.Hex
-
 object Protocol {
   val version = 0
-
-  object Genesis {
-    // scalastyle:off magic.number
-    val privateKey: ED25519PrivateKey = ED25519PrivateKey.unsafeFrom(
-      Hex("604b105965f2bb262d5bede6f9790c7ba9ca08c0f31627ec24f52b67b59dfa65"))
-    val publicKey: ED25519PublicKey = ED25519PublicKey.unsafeFrom(
-      Hex("2db399c90fee96ec2310b62e3f62b5bd87972a96e5fa64675f0adc683546cd1c"))
-    val balance = 100
-
-    val unsigned     = UnsignedTransaction(Seq.empty, Seq(TxOutput(balance, publicKey)))
-    val transaction  = Transaction(unsigned, ED25519Signature.zero)
-    val block: Block = Block.from(Seq.empty, Seq(transaction))
-    // scalastyle:on
-  }
 }
