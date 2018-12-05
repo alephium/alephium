302743789acf3afcc5f2826ac6b7c5101cafdc82
Cheng Wang <chengwang0511@gmail.com>
2018-12-05 13:52:08 +0100

    Add proper config for testing actor system

--- src/test/scala/org/alephium/AlephiumActorSpec.scala
+++ src/test/scala/org/alephium/AlephiumActorSpec.scala
@@ -2,13 +2,30 @@ package org.alephium
 
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
+import com.typesafe.config.ConfigFactory
 import org.scalatest.BeforeAndAfterAll
 
 class AlephiumActorSpec(name: String)
-    extends TestKit(ActorSystem(name))
+    extends TestKit(ActorSystem(name, ConfigFactory.parseString(AlephiumActorSpec.config)))
     with AlephiumSpec
     with BeforeAndAfterAll {
   override def afterAll: Unit = {
     TestKit.shutdownActorSystem(system)
   }
+}
+
+object AlephiumActorSpec {
+  val config: String =
+    """
+      |akka {
+      |  stdout-loglevel = "OFF"
+      |  loglevel = "WARNING"
+      |
+      |  actor {
+      |    debug {
+      |      unhandled = on
+      |    }
+      |  }
+      |}
+    """.stripMargin
 }
