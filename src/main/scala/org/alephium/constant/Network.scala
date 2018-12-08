6a81382c0ed83b831a2b6de5e469c8970ff77710
Cheng Wang <wachmc@gmail.com>
2018-12-08 20:30:11 +0100

    Refactor configuration

--- src/main/scala/org/alephium/constant/Network.scala
+++ src/main/scala/org/alephium/constant/Network.scala
@@ -1,9 +1,26 @@
 package org.alephium.constant
 
-import scala.concurrent.duration._
-import scala.language.postfixOps
+import java.net.InetSocketAddress
+import java.time.Duration
+
+import com.typesafe.config.ConfigFactory
 
 object Network {
-  val port: Int                     = 9973
-  val pingFrequency: FiniteDuration = 5 minute
+  private val config = ConfigFactory.load().getConfig("alephium")
+
+  val port: Int               = config.getInt("port")
+  val pingFrequency: Duration = config.getDuration("pingFrequency")
+
+  val peers: Seq[InetSocketAddress] = {
+    val rawInput = config.getString("peers")
+    if (rawInput.isEmpty) Seq.empty
+    else {
+      val addresses = rawInput.split(';')
+      addresses.map { address =>
+        val host = address.takeWhile(_ != ':')
+        val port = address.dropWhile(_ != ':').tail.toInt
+        new InetSocketAddress(host, port)
+      }
+    }
+  }
 }
