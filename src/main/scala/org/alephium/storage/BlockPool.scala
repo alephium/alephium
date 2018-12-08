85a7528e3ac1646b89c9eefc9ca71e0f8701fe8f
Cheng Wang <wachmc@gmail.com>
2018-12-08 08:30:37 +0100

    Refactor block pool

--- src/main/scala/org/alephium/storage/BlockPool.scala
+++ src/main/scala/org/alephium/storage/BlockPool.scala
@@ -35,6 +35,69 @@ trait AbsBlockPool {
   def getHeight: Int = getBestChain.size
 
   def getAllHeaders: Seq[Keccak256]
+
+  // TODO: have a safe version
+  def getTransaction(hash: Keccak256): Transaction
+
+  def getTxInputValue(transaction: Transaction, address: ED25519PublicKey): BigInt = {
+    transaction.unsigned.inputs.map {
+      case TxInput(txHash, outputIndex) =>
+        val tx       = getTransaction(txHash)
+        val txOutput = tx.unsigned.outputs(outputIndex)
+        if (txOutput.publicKey == address) txOutput.value else BigInt(0)
+    }.sum
+  }
+
+  def getTxOutputValue(transaction: Transaction, address: ED25519PublicKey): BigInt = {
+    transaction.unsigned.outputs.filter(_.publicKey == address).map(_.value).sum
+  }
+
+  def getBalance(transaction: Transaction, address: ED25519PublicKey): BigInt = {
+    getTxOutputValue(transaction, address) - getTxInputValue(transaction, address)
+  }
+
+  def getBalance(block: Block, address: ED25519PublicKey): BigInt = {
+    block.transactions.map(transaction => getBalance(transaction, address)).sum
+  }
+
+  def getUTXOs(address: ED25519PublicKey): (Keccak256, Seq[TxInput]) = {
+    val bestChain = getBestChain
+    val txosOI = for {
+      block             <- bestChain
+      transaction       <- block.transactions
+      (txOutput, index) <- transaction.unsigned.outputs.zipWithIndex
+      if txOutput.publicKey == address
+    } yield TxInput(transaction.hash, index)
+    val stxosOI = for {
+      block       <- bestChain
+      transaction <- block.transactions
+      txInput     <- transaction.unsigned.inputs
+    } yield txInput
+    (bestChain.last.hash, txosOI diff stxosOI)
+  }
+
+  // return a number of inputs with at lease value Aleph
+  def getUTXOs(address: ED25519PublicKey,
+               value: BigInt): Option[(Keccak256, Seq[TxInput], BigInt)] = {
+    val (header, utxos) = getUTXOs(address)
+    val values = utxos
+      .scanLeft(BigInt(0)) {
+        case (acc, TxInput(txHash, outputIndex)) =>
+          val tx       = getTransaction(txHash)
+          val txOutput = tx.unsigned.outputs(outputIndex)
+          acc + txOutput.value
+      }
+      .tail
+    val index = values.indexWhere(_ >= value)
+    if (index == -1) None else Some((header, utxos.take(index + 1), values(index)))
+  }
+
+  // calculated from best chain
+  def getBalance(address: ED25519PublicKey): (Block, BigInt) = {
+    val bestHeader = getBestHeader
+    val balance    = getBestChain.map(block => getBalance(block, address)).sum
+    (bestHeader, balance)
+  }
 }
 
 class BlockPool extends AbsBlockPool {
@@ -76,63 +139,5 @@ class BlockPool extends AbsBlockPool {
     blockStore.values.filter(isHeader).map(_.hash).toSeq
   }
 
-  def getTxInputValue(transaction: Transaction, address: ED25519PublicKey): BigInt = {
-    transaction.unsigned.inputs.map {
-      case TxInput(txHash, outputIndex) =>
-        val tx       = txStore(txHash)
-        val txOutput = tx.unsigned.outputs(outputIndex)
-        if (txOutput.publicKey == address) txOutput.value else BigInt(0)
-    }.sum
-  }
-
-  def getTxOutputValue(transaction: Transaction, address: ED25519PublicKey): BigInt = {
-    transaction.unsigned.outputs.filter(_.publicKey == address).map(_.value).sum
-  }
-
-  def getBalance(transaction: Transaction, address: ED25519PublicKey): BigInt = {
-    getTxOutputValue(transaction, address) - getTxInputValue(transaction, address)
-  }
-
-  def getBalance(block: Block, address: ED25519PublicKey): BigInt = {
-    block.transactions.map(transaction => getBalance(transaction, address)).sum
-  }
-
-  def getUTXOs(address: ED25519PublicKey): (Keccak256, Seq[TxInput]) = {
-    val bestChain = getBestChain
-    val txosOI = for {
-      block             <- bestChain
-      transaction       <- block.transactions
-      (txOutput, index) <- transaction.unsigned.outputs.zipWithIndex
-      if txOutput.publicKey == address
-    } yield TxInput(transaction.hash, index)
-    val stxosOI = for {
-      block       <- bestChain
-      transaction <- block.transactions
-      txInput     <- transaction.unsigned.inputs
-    } yield txInput
-    (bestChain.last.hash, txosOI diff stxosOI)
-  }
-
-  // return a number of inputs with at lease value Aleph
-  def getUTXOs(address: ED25519PublicKey,
-               value: BigInt): Option[(Keccak256, Seq[TxInput], BigInt)] = {
-    val (header, utxos) = getUTXOs(address)
-    val values = utxos
-      .scanLeft(BigInt(0)) {
-        case (acc, TxInput(txHash, outputIndex)) =>
-          val tx       = txStore(txHash)
-          val txOutput = tx.unsigned.outputs(outputIndex)
-          acc + txOutput.value
-      }
-      .tail
-    val index = values.indexWhere(_ >= value)
-    if (index == -1) None else Some((header, utxos.take(index + 1), values(index)))
-  }
-
-  // calculated from best chain
-  def getBalance(address: ED25519PublicKey): (Block, BigInt) = {
-    val bestHeader = getBestHeader
-    val balance    = getBestChain.map(block => getBalance(block, address)).sum
-    (bestHeader, balance)
-  }
+  override def getTransaction(hash: Keccak256): Transaction = txStore(hash)
 }
