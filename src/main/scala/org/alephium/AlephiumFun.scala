70a0e5c77604e4f4063707a99e6a9701c6823fd1
Cheng Wang <chengwang0511@gmail.com>
2018-12-09 09:37:04 +0100

    Small fixes

--- src/main/scala/org/alephium/AlephiumFun.scala
+++ src/main/scala/org/alephium/AlephiumFun.scala
@@ -14,12 +14,14 @@ object AlephiumFun extends App {
   val node = Node("ClientFun", Network.port)
   import node.{peerManager, system}
 
+  Thread.sleep(1000 * 20)
+
   (9973 to 9976).filter(_ != Network.port).foreach { peerPort =>
     val remote = new InetSocketAddress("localhost", peerPort)
     peerManager ! PeerManager.Connect(remote)
   }
 
-  Thread.sleep(1000 * 60)
+  Thread.sleep(1000 * 20)
 
   val publicKey: ED25519PublicKey = ED25519PublicKey.unsafeFrom(
     hex"2db399c90fee96ec2310b62e3f62b5bd87972a96e5fa64675f0adc683546cd1d")
