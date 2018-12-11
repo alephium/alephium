687990dcd36fc82cfdec1c82ed20f8399c7fc986
Alois Cochard <alois.cochard@gmail.com>
2018-12-11 23:36:48 +0100

    Extract builders for `TcpHandler` and `MessageHandler`.

--- shared/src/main/scala/org/alephium/client/Node.scala
+++ shared/src/main/scala/org/alephium/client/Node.scala
@@ -1,7 +1,7 @@
 package org.alephium.client
 
-import akka.actor.{ActorRef, ActorSystem}
-import org.alephium.network.PeerManager
+import akka.actor.{ActorRef, ActorSystem, Props}
+import org.alephium.network.{PeerManager, TcpHandler, MessageHandler}
 import org.alephium.storage.BlockFlow.ChainIndex
 import org.alephium.storage.{BlockFlow, BlockHandlers, ChainHandler, FlowHandler}
 
@@ -14,10 +14,12 @@ case class Node(
 )
 
 object Node {
-  def apply(name: String, port: Int, groups: Int): Node = {
+  def apply(builders: TcpHandler.Builder with MessageHandler.Builder,
+            name: String, port: Int, groups: Int): Node = {
+
     val system      = ActorSystem(name)
     val blockFlow   = BlockFlow()
-    val peerManager = system.actorOf(PeerManager.props(port), "PeerManager")
+    val peerManager = system.actorOf(Props(new PeerManager(builders, port)), "PeerManager")
 
     val blockHandler = system.actorOf(FlowHandler.props(blockFlow), "BlockHandler")
     val chainHandlers = Seq.tabulate(groups, groups) {
