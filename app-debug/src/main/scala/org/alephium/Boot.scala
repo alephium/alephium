719e683794b98aff4605cf093af1ad866585f07b
Alois Cochard <alois.cochard@gmail.com>
2018-12-12 20:06:36 +0100

    Set custom `Mode` in `app-debug`.

--- app-debug/src/main/scala/org/alephium/Boot.scala
+++ app-debug/src/main/scala/org/alephium/Boot.scala
@@ -1,3 +1,22 @@
 package org.alephium
 
-object Boot extends Platform
+import java.net.InetSocketAddress
+import akka.actor.{ActorRef, Props}
+
+import org.alephium.network.{TcpHandler, MessageHandler}
+import org.alephium.storage.BlockHandlers
+
+object Boot extends Platform {
+  override val mode = new Mode.Local {
+    override def builders: TcpHandler.Builder with MessageHandler.Builder =
+      new TcpHandler.Builder with MessageHandler.Builder {
+        override def MessageHandler(remote: InetSocketAddress, connection: ActorRef, blockHandlers: BlockHandlers): Props =
+          Props(new MessageHandler(remote, connection, blockHandlers) {
+            override def handlePing(nonce: Int, timestamp: Long): Unit = {
+              super.handlePing(nonce, timestamp)
+              // TODO Add metrics monitoring logic
+            }
+          })
+      }
+  }
+}
