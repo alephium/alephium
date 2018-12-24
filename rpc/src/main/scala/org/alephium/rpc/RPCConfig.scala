07f482e25422820e19740dcae23e2dfe526ec506
Alois Cochard <alois.cochard@gmail.com>
2018-12-24 01:44:48 +0100

    Initial `JsonRPC` implementation.

--- rpc/src/main/scala/org/alephium/rpc/RPCConfig.scala
+++ rpc/src/main/scala/org/alephium/rpc/RPCConfig.scala
@@ -3,11 +3,14 @@ package org.alephium.rpc
 import com.typesafe.config.Config
 import java.time.Duration
 
-case class RPCConfig(viewerBlockAgeLimit: Duration)
+case class RPCConfig(
+    networkInterface: String,
+    blockflowFetchMaxAge: Duration
+)
 
 object RPCConfig {
   def load(implicit config: Config): RPCConfig = {
     val rpc = config.getConfig("rpc")
-    RPCConfig(rpc.getDuration("viewerBlockAgeLimit"))
+    RPCConfig(rpc.getString("network.interface"), rpc.getDuration("blockflowFetch.maxAge"))
   }
 }
--- rpc/src/main/scala/org/alephium/rpc/RPCServer.scala
+++ rpc/src/main/scala/org/alephium/rpc/RPCServer.scala
@@ -1,20 +1,17 @@
 package org.alephium.rpc
 
+import scala.concurrent.Future
 import java.time.Instant
 
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.model.ws.{Message, TextMessage}
-import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Flow
 import com.typesafe.scalalogging.StrictLogging
-import io.circe.parser._
+import io.circe.Json
+
 import org.alephium.flow.client.{FairMiner, Miner, Node}
 import org.alephium.flow.{Mode, Platform}
-import org.alephium.rpc.model.ViewerQuery
-
-import scala.concurrent.Future
+import org.alephium.protocol.config.ConsensusConfig
+import org.alephium.rpc.model.JsonRPC
 
 trait RPCServer extends Platform with CORSHandler with StrictLogging {
   import RPCServer._
@@ -30,37 +27,45 @@ trait RPCServer extends Platform with CORSHandler with StrictLogging {
     implicit val config           = mode.config
     implicit val rpcConfig        = RPCConfig.load(config.alephium)
 
-    val miningRoute = path("mining") {
-      put {
-        val publicKey = config.discoveryConfig.discoveryPublicKey
-        val props     = FairMiner.props(publicKey, node).withDispatcher("akka.actor.mining-dispatcher")
-        val miner     = system.actorOf(props, s"FairMiner")
-        miner ! Miner.Start
-
-        complete((StatusCodes.Accepted, "Start mining"))
-      }
+    val miner = {
+      val publicKey = config.discoveryConfig.discoveryPublicKey
+      val props     = FairMiner.props(publicKey, node).withDispatcher("akka.actor.mining-dispatcher")
+      system.actorOf(props, s"FairMiner")
     }
 
-    val viewerRoute = corsHandler(path("viewer") {
-      get {
-        handleWebSocketMessages(wsViewer(node))
-      }
-    })
+    val rpcHandler: JsonRPC.Handler = {
+      case "blockflow/fetch" =>
+        req =>
+          blockflowFetch(node, req)
 
-    val route = miningRoute ~ viewerRoute
+      case "mining/start" =>
+        req =>
+          miner ! Miner.Start
+          req.success(Json.Null)
 
-    Http().bindAndHandle(route, "0.0.0.0", mode.httpPort).map(_ => ())
+      case "mining/stop" =>
+        req =>
+          miner ! Miner.Stop
+          req.success(Json.Null)
+    }
+
+    val route = corsHandler(JsonRPCHandler.route(rpcHandler))
+
+    Http().bindAndHandle(route, rpcConfig.networkInterface, mode.httpPort).map(_ => ())
   }
 }
 
 object RPCServer extends StrictLogging {
-  private val failure = TextMessage("""{"error": "Bad request"}"""")
+  import JsonRPC._
 
-  private def handleQuery(text: String, node: Node)(implicit rpc: RPCConfig) = {
-    decode[ViewerQuery](text) match {
+  def blockflowFetch(node: Node, req: Request)(implicit rpc: RPCConfig,
+                                               cfg: ConsensusConfig): Response = {
+    import model.BlockFlowRPC._
+
+    req.params.as[FetchRequest] match {
       case Right(query) =>
         val now        = Instant.now()
-        val lowerBound = now.minus(rpc.viewerBlockAgeLimit).toEpochMilli
+        val lowerBound = now.minus(rpc.blockflowFetchMaxAge).toEpochMilli
 
         val from = query.from match {
           case Some(ts) => Math.max(ts, lowerBound)
@@ -68,24 +73,14 @@ object RPCServer extends StrictLogging {
         }
 
         val headers = node.blockFlow.getHeadersUnsafe(header => header.timestamp > from)
-        val json = headers
-          .map(node.blockFlow.toJson)
-          .mkString("""{"blocks":[""", ",", "]}")
-        TextMessage(json)
+        val blocks  = headers.map(blockHeaderEncoder(node.blockFlow).apply)
 
-      case Left(e) =>
-        logger.info(s"Decode error for request test: $text: ${e.toString}")
-        failure
-    }
-  }
+        val json = Json.obj(("blocks", Json.arr(blocks: _*)))
 
-  def wsViewer(node: Node)(implicit rpc: RPCConfig): Flow[Message, Message, Any] = {
-    Flow[Message].collect {
-      case request: TextMessage.Strict =>
-        handleQuery(request.text, node)
-      case request: TextMessage.Streamed =>
-        logger.info(s"Received non-valid RPC viewer request: $request")
-        failure
+        req.success(json)
+      case Left(decodingFailure) =>
+        logger.debug(s"Unable to decode blokflow/fetch request. (${decodingFailure})")
+        req.failure(Error.InvalidParams)
     }
   }
 }
