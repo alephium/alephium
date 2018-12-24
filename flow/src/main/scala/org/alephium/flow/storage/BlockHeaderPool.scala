8e44d1459c1ad47a5c02e11b7b46f1e55d0f8c9e
Cheng Wang <wachmc@gmail.com>
2018-12-24 11:56:54 +0100

    Refactor validation error

--- flow/src/main/scala/org/alephium/flow/storage/BlockHeaderPool.scala
+++ flow/src/main/scala/org/alephium/flow/storage/BlockHeaderPool.scala
@@ -1,7 +1,7 @@
 package org.alephium.flow.storage
 
 import org.alephium.crypto.Keccak256
-import org.alephium.flow.io.{IOError, IOResult}
+import org.alephium.flow.io.IOResult
 import org.alephium.protocol.model.BlockHeader
 import org.alephium.util.AVector
 
@@ -26,32 +26,4 @@ trait BlockHeaderPool extends BlockHashPool {
   def getWeight(bh: BlockHeader): Int = getWeight(bh.hash)
 
   def isTip(bh: BlockHeader): Boolean = isTip(bh.hash)
-}
-
-sealed trait AddBlockHeaderResult
-
-object AddBlockHeaderResult {
-  sealed trait OK     extends AddBlockHeaderResult
-  case object Success extends OK
-  case object AlreadyExisted extends OK {
-    override def toString: String = "BlockHeader already exist"
-  }
-
-  trait Error                                 extends AddBlockHeaderResult
-  sealed trait VerificationError              extends Error
-  case class IOErrorForHeader(error: IOError) extends Error
-  case class Other(message: String) extends Error {
-    override def toString: String = s"Failed in adding blockheader: $message"
-  }
-
-  case object InvalidGroup extends VerificationError {
-    override def toString: String = "Block index is related to node's group"
-  }
-  case object InvalidDifficulty extends VerificationError {
-    override def toString: String = "Difficulty is invalid"
-  }
-  case class MissingDeps(deps: AVector[Keccak256]) extends VerificationError {
-    override def toString: String = s"Missing #$deps.length deps"
-  }
-
 }
