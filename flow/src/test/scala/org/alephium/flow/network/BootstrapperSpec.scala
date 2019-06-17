e409e28a36724efb427b49831973b2e9e307818e
Cheng Wang <wachmc@gmail.com>
2019-06-17 14:36:14 +0200

    wip

--- flow/src/test/scala/org/alephium/flow/network/BootstrapperSpec.scala
+++ flow/src/test/scala/org/alephium/flow/network/BootstrapperSpec.scala
@@ -2,7 +2,6 @@ package org.alephium.flow.network
 
 import akka.testkit.TestProbe
 import org.alephium.flow.PlatformConfig
-import org.alephium.flow.network.clique.BrokerHandler
 import org.alephium.protocol.model.ModelGen
 import org.alephium.util.AlephiumActorSpec
 
@@ -13,10 +12,7 @@ class BootstrapperSpec extends AlephiumActorSpec("BootstrapperSpec") {
     val cliqueManagerProb   = TestProbe()
 
     val bootstrapper = system.actorOf(
-      Bootstrapper.props(new BrokerHandler.Builder {},
-                         serverProb.ref,
-                         discoveryServerProb.ref,
-                         cliqueManagerProb.ref))
+      Bootstrapper.props(serverProb.ref, discoveryServerProb.ref, cliqueManagerProb.ref))
 
     val cliqueInfo = ModelGen.cliqueInfo.sample.get
     bootstrapper ! cliqueInfo
