9e497fce02a8debbab14903cb6a8305ffbce35e0
Cheng Wang <wachmc@gmail.com>
2019-06-17 23:54:49 +0200

    Add more tests

--- protocol/src/main/scala/org/alephium/protocol/model/BrokerId.scala
+++ protocol/src/main/scala/org/alephium/protocol/model/BrokerId.scala
@@ -5,29 +5,29 @@ import org.alephium.serde.{Serde, Serializer}
 
 class BrokerId private (val value: Int) extends AnyVal {
   def contains(index: GroupIndex)(implicit config: CliqueConfig): Boolean = {
-    index.value >= (value * config.groupNumPerBroker) && index.value < (value + 1) * config.groupNumPerBroker
+    containsRaw(index.value)
   }
 
   def containsRaw(index: Int)(implicit config: CliqueConfig): Boolean = {
-    index >= (value * config.groupNumPerBroker) && index < (value + 1) * config.groupNumPerBroker
+    (value * config.groupNumPerBroker) <= index && index < (value + 1) * config.groupNumPerBroker
   }
 
   def intersect(cliqueInfo: CliqueInfo, brokerId: BrokerId)(
       implicit config: BrokerConfig): Boolean = {
     val groupFrom  = cliqueInfo.groupNumPerBroker * brokerId.value
     val groupUntil = groupFrom + cliqueInfo.groupNumPerBroker
-    !(groupUntil < config.groupFrom || groupFrom > config.groupUntil)
+    BrokerId.intersect(groupFrom, groupUntil, config.groupFrom, config.groupUntil)
   }
 
   override def toString: String = s"BrokerId($value)"
 }
 
-object BrokerId {
+object BrokerId { self =>
   implicit val serializer: Serializer[BrokerId] = Serializer.forProduct1(_.value)
 
   class Unsafe(val value: Int) extends AnyVal {
     def validate(implicit config: CliqueConfig): Either[String, BrokerId] = {
-      if (0 <= value && value < config.brokerNum) Right(BrokerId.unsafe(value))
+      if (self.validate(value)) Right(BrokerId.unsafe(value))
       else Left("Invalid value for broker id")
     }
   }
@@ -44,4 +44,9 @@ object BrokerId {
 
   def validate(brokerId: Int)(implicit config: CliqueConfig): Boolean =
     0 <= brokerId && brokerId < config.brokerNum
+
+  // Check if two segments intersect or not
+  @inline def intersect(from0: Int, until0: Int, from1: Int, until1: Int): Boolean = {
+    !(until0 <= from1 || until1 <= from0)
+  }
 }
