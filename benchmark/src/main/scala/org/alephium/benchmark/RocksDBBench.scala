3ac0b5d3d765be5064ef4fcabb07bff904d1135a
Alois Cochard <alois.cochard@gmail.com>
2019-06-27 20:47:07 +0200

    Enrich the `RocksDBBench` suite with memory budget alternatives.
    Benchmark                          Mode  Cnt      Score      Error  Units
    RocksDBBench.nothingSettings         ss    5  24897.537 ± 2941.170  ms/op
    RocksDBBench.ssdSettings             ss    5  28233.574 ± 1239.871  ms/op
    RocksDBBench.ssdSettingsFor128       ss    5  23467.473 ± 1116.456  ms/op
    RocksDBBench.ssdSettingsFor256       ss    5  22871.437 ± 1249.772  ms/op
    RocksDBBench.ssdSettingsFor512       ss    5  19075.379 ± 1645.333  ms/op

--- benchmark/src/main/scala/org/alephium/benchmark/RocksDBBench.scala
+++ benchmark/src/main/scala/org/alephium/benchmark/RocksDBBench.scala
@@ -3,39 +3,31 @@ package org.alephium.benchmark
 import scala.util.Random
 import java.util.concurrent.TimeUnit
 
+import org.rocksdb.{ColumnFamilyOptions, DBOptions, RocksDB}
+import org.rocksdb.util.SizeUnit
+
 import org.alephium.crypto.Keccak256
 import org.alephium.flow.io.{RocksDBColumn, RocksDBStorage}
 import org.alephium.util.Files
 import org.openjdk.jmh.annotations._
 
-import RocksDBStorage.ColumnFamily
+import RocksDBStorage.{ColumnFamily, Compaction, Settings}
 
 @BenchmarkMode(Array(Mode.SingleShotTime))
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Thread)
 // scalastyle:off
 class RocksDBBench {
-
-  private val random = new Random(0)
-
-  private val N = 1000000
-
-  private val tmpdir = Files.tmpDir
-  private val dbPath = tmpdir.resolve(RocksDBStorage.ColumnFamily.All.name)
-
-  val dbStorage: RocksDBStorage = {
-    val files = dbPath.toFile.listFiles
-    if (files != null) {
-      files.foreach(_.delete)
-    }
-
-    RocksDBStorage.openUnsafe(dbPath, RocksDBStorage.Compaction.SSD)
+  {
+    RocksDB.loadLibrary()
   }
 
-  val db: RocksDBColumn = RocksDBColumn(dbStorage, ColumnFamily.All, dbStorage.readOptions)
+  private val N      = 1000000
+  private val tmpdir = Files.tmpDir
 
-  @Benchmark
-  def randomInsertAndLookup(): Unit = {
+  def randomInsertAndLookup(db: RocksDBColumn): Unit = {
+    val random = new Random(0)
+
     // Insert N
     val keys = (0 to N).map { _ =>
       val bytes = Keccak256.random.bytes
@@ -62,5 +54,66 @@ class RocksDBBench {
       val key = randomKey()
       db.getRaw(key)
     }
+  }
+
+  def createDB(name: String,
+               databaseOptions: DBOptions,
+               columnOptions: ColumnFamilyOptions): RocksDBColumn = {
+    val path = tmpdir.resolve(s"bench-$name")
+
+    val files = path.toFile.listFiles
+    if (files != null) {
+      files.foreach(_.delete)
+    }
+
+    val storage: RocksDBStorage =
+      RocksDBStorage.openUnsafeWithOptions(path, databaseOptions, columnOptions)
+
+    RocksDBColumn(storage, ColumnFamily.All, storage.readOptions)
+  }
+
+  def createDBForBudget(name: String, memoryBudgetPerCol: Long): RocksDBColumn =
+    createDB(name,
+             Settings.databaseOptionsForBudget(Compaction.SSD, memoryBudgetPerCol),
+             Settings.columnOptionsForBudget(Compaction.SSD, memoryBudgetPerCol))
+
+  @Benchmark
+  def nothingSettings(): Unit = {
+    val db: RocksDBColumn =
+      createDB("nothing",
+               new DBOptions()
+                 .setCreateIfMissing(true)
+                 .setCreateMissingColumnFamilies(true),
+               new ColumnFamilyOptions)
+
+    randomInsertAndLookup(db)
+  }
+
+  @Benchmark
+  def ssdSettings(): Unit = {
+    val db: RocksDBColumn =
+      createDB("ssd",
+               Settings.databaseOptions(Compaction.SSD),
+               Settings.columnOptions(Compaction.SSD))
+
+    randomInsertAndLookup(db)
+  }
+
+  @Benchmark
+  def ssdSettingsFor128(): Unit = {
+    val db: RocksDBColumn = createDBForBudget("ssd-128", 128 * SizeUnit.MB)
+    randomInsertAndLookup(db)
+  }
+
+  @Benchmark
+  def ssdSettingsFor256(): Unit = {
+    val db: RocksDBColumn = createDBForBudget("ssd-256", 256 * SizeUnit.MB)
+    randomInsertAndLookup(db)
+  }
+
+  @Benchmark
+  def ssdSettingsFor512(): Unit = {
+    val db: RocksDBColumn = createDBForBudget("ssd-512", 512 * SizeUnit.MB)
+    randomInsertAndLookup(db)
   }
 }
