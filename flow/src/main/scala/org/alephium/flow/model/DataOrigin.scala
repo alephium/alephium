40c8924bb0fdaef181e3dde4d4547148577c47f0
Cheng Wang <wachmc@gmail.com>
2019-07-14 18:27:18 +0200

    broadcast headers as well

--- flow/src/main/scala/org/alephium/flow/model/DataOrigin.scala
+++ flow/src/main/scala/org/alephium/flow/model/DataOrigin.scala
@@ -1,16 +1,23 @@
 package org.alephium.flow.model
 
-import org.alephium.protocol.model.CliqueId
+import org.alephium.protocol.model.{BrokerInfo, CliqueId}
 
 sealed trait DataOrigin {
   def isFrom(another: CliqueId): Boolean
+
+  def isFrom(cliqueId: CliqueId, brokerInfo: BrokerInfo): Boolean
 }
 
 object DataOrigin {
   case object LocalMining extends DataOrigin {
     override def isFrom(another: CliqueId): Boolean = false
+
+    override def isFrom(cliqueId: CliqueId, brokerInfo: BrokerInfo): Boolean = false
   }
-  case class Remote(cliqueId: CliqueId) extends DataOrigin {
+  case class Remote(cliqueId: CliqueId, brokerInfo: BrokerInfo) extends DataOrigin {
     override def isFrom(another: CliqueId): Boolean = cliqueId == another
+
+    override def isFrom(_cliqueId: CliqueId, _brokerInfo: BrokerInfo): Boolean =
+      cliqueId == _cliqueId && _brokerInfo == brokerInfo
   }
 }
