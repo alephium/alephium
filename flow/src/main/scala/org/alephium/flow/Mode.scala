82fc1117b3cd11555d6d8d10d7f36067d06a21c3
Alois Cochard <alois.cochard@gmail.com>
2019-09-02 12:09:45 +0200

    Compute `rpcPort` from TCP `port` in every mode.

--- flow/src/main/scala/org/alephium/flow/Mode.scala
+++ flow/src/main/scala/org/alephium/flow/Mode.scala
@@ -8,7 +8,7 @@ import org.alephium.flow.network.clique.BrokerHandler
 trait Mode extends PlatformConfig.Default {
   def port: Int
 
-  def httpPort: Int
+  def rpcPort: Int = port + 1000
 
   def builders: Mode.Builder = Mode.defaultBuilders
 
@@ -24,15 +24,11 @@ object Mode {
   class Aws extends Mode with StrictLogging {
     val port: Int = config.publicAddress.getPort
 
-    val httpPort: Int = 8080
-
     override val node: Node = Node(builders, "Root")
   }
 
   class Local extends Mode {
     val port: Int = config.publicAddress.getPort
-
-    def httpPort: Int = 8080 + (port - 9973)
 
     override val node: Node = Node(builders, "Root")
   }
