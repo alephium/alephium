d56058838f74df7aad3608ef4e6e59b9e2681b73
Cheng Wang <wachmc@gmail.com>
2019-10-06 20:59:59 +0200

    Refactor more

---
+++ app-server/src/main/scala/org/alephium/RPCModel.scala
@@ -0,0 +1,30 @@
+package org.alephium
+
+import io.circe._
+import io.circe.generic.semiauto._
+
+object RPCModel {
+  case class FetchRequest(from: Option[Long])
+
+  object FetchRequest {
+    implicit val decoder: Decoder[FetchRequest] = deriveDecoder[FetchRequest]
+  }
+
+  case class FetchResponse(blocks: List[FetchEntry])
+  object FetchResponse {
+    implicit val encoder: Encoder[FetchResponse] = deriveEncoder[FetchResponse]
+  }
+
+  case class FetchEntry(
+      hash: String,
+      timestamp: Long,
+      chainFrom: Int,
+      chainTo: Int,
+      height: Int,
+      deps: List[String]
+  )
+
+  object FetchEntry {
+    implicit val encoder: Encoder[FetchEntry] = deriveEncoder[FetchEntry]
+  }
+}
