db20696988d2b3620de25c5c1b6b1baf3a2cc390
Cheng Wang <wachmc@gmail.com>
2019-11-08 16:51:43 +0100

    Renaming functions of Validation

--- flow/src/test/scala/org/alephium/flow/core/validation/ValidationSpec.scala
+++ flow/src/test/scala/org/alephium/flow/core/validation/ValidationSpec.scala
@@ -23,10 +23,10 @@ class ValidationSpec extends AlephiumFlowSpec {
 
   it should "validate group for block" in {
     forAll(ModelGen.blockGenFor(config.brokerInfo)) { block =>
-      check(Validation.validateGroup(block))
+      check(Validation.checkGroup(block))
     }
     forAll(ModelGen.blockGenNotFor(config.brokerInfo)) { block =>
-      check(Validation.validateGroup(block), InvalidGroup)
+      check(Validation.checkGroup(block), InvalidGroup)
     }
   }
 
@@ -38,19 +38,19 @@ class ValidationSpec extends AlephiumFlowSpec {
     val header0 = header.copy(timestamp = now)
     val header1 = header.copy(timestamp = before)
     val header2 = header.copy(timestamp = after)
-    check(Validation.validateTimeStamp(header0, isSyncing = false))
-    check(Validation.validateTimeStamp(header1, isSyncing = false), InvalidTimeStamp)
-    check(Validation.validateTimeStamp(header2, isSyncing = false), InvalidTimeStamp)
-    check(Validation.validateTimeStamp(header0, isSyncing = true))
-    check(Validation.validateTimeStamp(header1, isSyncing = true))
-    check(Validation.validateTimeStamp(header2, isSyncing = true), InvalidTimeStamp)
+    check(Validation.checkTimeStamp(header0, isSyncing = false))
+    check(Validation.checkTimeStamp(header1, isSyncing = false), InvalidTimeStamp)
+    check(Validation.checkTimeStamp(header2, isSyncing = false), InvalidTimeStamp)
+    check(Validation.checkTimeStamp(header0, isSyncing = true))
+    check(Validation.checkTimeStamp(header1, isSyncing = true))
+    check(Validation.checkTimeStamp(header2, isSyncing = true), InvalidTimeStamp)
   }
 
   it should "validate nonEmpty transaction list" in {
     val block0 = ModelGen.blockGen.retryUntil(_.transactions.nonEmpty).sample.get
     val block1 = block0.copy(transactions = AVector.empty)
-    check(Validation.validateNonEmptyTransactions(block0))
-    check(Validation.validateNonEmptyTransactions(block1), EmptyTransactionList)
+    check(Validation.checkNonEmptyTransactions(block0))
+    check(Validation.checkNonEmptyTransactions(block1), EmptyTransactionList)
   }
 
   it should "validate coinbase transaction" in {
@@ -69,36 +69,36 @@ class ValidationSpec extends AlephiumFlowSpec {
     val coinbase0 = block0.transactions.head
     val input0    = coinbase0.unsigned.inputs
     val output0   = coinbase0.unsigned.outputs
-    check(validateCoinbase(block0), InvalidCoinbase)
+    check(checkCoinbase(block0), InvalidCoinbase)
 
     val coinbase1 =
       Transaction.from0(AVector.empty, AVector(output0.head), privateKey)
     val block1 = block0.copy(transactions = AVector(coinbase1))
-    check(validateCoinbase(block1), InvalidCoinbase)
+    check(checkCoinbase(block1), InvalidCoinbase)
 
     val coinbase2 = Transaction.from0(AVector(input0.head), AVector(output0.head), privateKey)
     val block2    = block0.copy(transactions = AVector(coinbase2))
-    check(validateCoinbase(block2), InvalidCoinbase)
+    check(checkCoinbase(block2), InvalidCoinbase)
 
     val coinbase3 =
       Transaction.from0(AVector.empty, AVector.empty, privateKey)
     val block3 = block0.copy(transactions = AVector(coinbase3))
-    check(validateCoinbase(block3), InvalidCoinbase)
+    check(checkCoinbase(block3), InvalidCoinbase)
 
     val coinbase4 =
       Transaction.from1(AVector.empty, AVector(output0.head), ED25519Signature.zero)
     val block4 = block0.copy(transactions = AVector(coinbase4))
-    check(validateCoinbase(block4))
+    check(checkCoinbase(block4))
 
     val coinbase5 =
       Transaction.from1(AVector(input0.head), AVector(output0.head), ED25519Signature.zero)
     val block5 = block0.copy(transactions = AVector(coinbase5))
-    check(validateCoinbase(block5), InvalidCoinbase)
+    check(checkCoinbase(block5), InvalidCoinbase)
 
     val coinbase6 =
       Transaction.from1(AVector.empty, AVector.empty, ED25519Signature.zero)
     val block6 = block0.copy(transactions = AVector(coinbase6))
-    check(validateCoinbase(block6), InvalidCoinbase)
+    check(checkCoinbase(block6), InvalidCoinbase)
   }
 
   // TODO: Add more mocking test
