9d6f372a6e20b1a0dfb0ff92e3e5c00b6bbde2e3
Cheng Wang <wachmc@gmail.com>
2020-01-29 22:15:29 +0100

    Validate scripts for block

--- protocol/src/test/scala/org/alephium/protocol/script/ScriptSpec.scala
+++ protocol/src/test/scala/org/alephium/protocol/script/ScriptSpec.scala
@@ -1,6 +1,7 @@
 package org.alephium.protocol.script
 
 import akka.util.ByteString
+import org.scalatest.EitherValues._
 
 import org.alephium.crypto._
 import org.alephium.util.{AlephiumSpec, AVector}
@@ -18,12 +19,16 @@ class ScriptSpec extends AlephiumSpec {
         AVector[Instruction](OP_KECCAK256, OP_PUSH(pkHash.bytes), OP_EQUALVERIFY, OP_CHECKSIG))
       val priScript  = AVector[Instruction](OP_PUSH(pk.bytes))
       val signatures = AVector(signature.bytes)
+      val witness    = Witness(priScript, signatures)
+      val witness0   = Witness(priScript, AVector.empty)
+      val witness1   = Witness(priScript.init, signatures)
+      val witness2   = Witness(OP_PUSH(pk.bytes) +: priScript, signatures)
 
-      Script.run(data, pubScript, Witness(priScript, signatures)) is ExeSuccessful
-      Script.run(data0, pubScript, Witness(priScript, signatures)) is VerificationFailed
-      Script.run(data, pubScript, Witness(priScript, AVector.empty)) is a[NonCategorized]
-      Script.run(data, pubScript, Witness(priScript.init, signatures)) is a[NonCategorized]
-      Script.run(data, pubScript, Witness(OP_PUSH(pk.bytes) +: priScript, signatures)) is InvalidFinalState
+      Script.run(data, pubScript, witness).isRight is true
+      Script.run(data0, pubScript, witness).left.value is VerificationFailed
+      Script.run(data, pubScript, witness0).left.value is a[NonCategorized]
+      Script.run(data, pubScript, witness1).left.value is a[NonCategorized]
+      Script.run(data, pubScript, witness2).left.value is InvalidFinalState
     }
   }
 }
