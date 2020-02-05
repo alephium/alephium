3af9048df921643836f93a4c6dcefe997d725c47
Cheng Wang <wachmc@gmail.com>
2020-02-05 11:07:28 +0100

    Fix fair miners

--- protocol/src/main/scala/org/alephium/protocol/config/GroupConfig.scala
+++ protocol/src/main/scala/org/alephium/protocol/config/GroupConfig.scala
@@ -1,24 +1,5 @@
 package org.alephium.protocol.config
 
-import scala.annotation.tailrec
-
-import org.alephium.crypto.{ED25519, ED25519PrivateKey, ED25519PublicKey}
-import org.alephium.protocol.model.GroupIndex
-
-object GroupConfig {
-  def generateKeyForGroup(groupIndex: GroupIndex)(
-      implicit config: GroupConfig): (ED25519PrivateKey, ED25519PublicKey) = {
-    @tailrec
-    def iter(): (ED25519PrivateKey, ED25519PublicKey) = {
-      val keyPair   = ED25519.generatePriPub()
-      val publicKey = keyPair._2
-      val keyIndex  = GroupIndex((publicKey.bytes.last & 0xFF) % config.groups)
-      if (keyIndex == groupIndex) keyPair else iter()
-    }
-    iter()
-  }
-}
-
 trait GroupConfig {
   def groups: Int
 
