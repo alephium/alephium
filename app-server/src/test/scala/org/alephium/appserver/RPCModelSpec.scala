40d04c41a66441e98e20f7cbeeeb6bab6a575977
Cheng Wang <wachmc@gmail.com>
2020-02-14 17:58:56 +0100

    Updates

--- app-server/src/test/scala/org/alephium/appserver/RPCModelSpec.scala
+++ app-server/src/test/scala/org/alephium/appserver/RPCModelSpec.scala
@@ -10,7 +10,7 @@ import org.alephium.crypto.ED25519PublicKey
 import org.alephium.util.{AlephiumSpec, Hex, TimeStamp}
 
 class RPCModelSpec extends AlephiumSpec with EitherValues {
-  val printer = org.alephium.rpc.JsonRPCHandler.printer
+  val printer = org.alephium.rpc.CirceUtils.printer
   def show[T](t: T)(implicit encoder: Encoder[T]): String = {
     printer.print(t.asJson)
   }
@@ -23,34 +23,34 @@ class RPCModelSpec extends AlephiumSpec with EitherValues {
     json.as[A].right.value
   }
 
-  def check[T](obj: T, jsonRaw: String)(implicit codec: Codec[T]): Assertion = {
-    show(obj) is jsonRaw
-    parseAs[T](jsonRaw) is obj
+  def checkData[T](data: T, jsonRaw: String)(implicit codec: Codec[T]): Assertion = {
+    show(data) is jsonRaw
+    parseAs[T](jsonRaw) is data
   }
 
   it should "encode/decode empty request" in {
     val request = FetchRequest(None)
     val jsonRaw = """{}"""
-    check(request, jsonRaw)
+    checkData(request, jsonRaw)
   }
 
   it should "encode/decode request" in {
     val request = FetchRequest(Some(TimeStamp.fromMillis(42L)))
     val jsonRaw = """{"from":42}"""
-    check(request, jsonRaw)
+    checkData(request, jsonRaw)
   }
 
   it should "encode/decode empty FetchResponse" in {
     val response = FetchResponse(Seq.empty)
     val jsonRaw  = """{"blocks":[]}"""
-    check(response, jsonRaw)
+    checkData(response, jsonRaw)
   }
 
   it should "encode/decode FetchResponse" in {
     val response = FetchResponse((0 to 1).map(entryDummy))
     val jsonRaw =
       """{"blocks":[{"hash":"0","timestamp":0,"chainFrom":0,"chainTo":0,"height":0,"deps":["0"]},{"hash":"1","timestamp":1,"chainFrom":1,"chainTo":1,"height":1,"deps":["1"]}]}"""
-    check(response, jsonRaw)
+    checkData(response, jsonRaw)
   }
 
   it should "encode/decode GetBalance" in {
@@ -58,12 +58,12 @@ class RPCModelSpec extends AlephiumSpec with EitherValues {
     val addressHex = Hex.toHexString(address.bytes)
     val request    = GetBalance(addressHex, GetBalance.pkh)
     val jsonRaw    = s"""{"address":"$addressHex","type":"${GetBalance.pkh}"}"""
-    check(request, jsonRaw)
+    checkData(request, jsonRaw)
   }
 
   it should "encode/decode Balance" in {
     val response = Balance(100, 1)
     val jsonRaw  = """{"balance":100,"utxoNum":1}"""
-    check(response, jsonRaw)
+    checkData(response, jsonRaw)
   }
 }
