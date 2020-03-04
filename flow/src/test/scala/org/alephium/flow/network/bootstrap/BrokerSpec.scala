7177cd4e446d4aa4340b4f2cefe26e1fee48f6e5
Thomas Droxler <thomas.droxler@gmail.com>
2020-03-04 08:52:16 +0100

    Improve `BrokerSpec`
    The starting point was an unhandled `Connection Failed` message received
    while being in the `awaitCliqueInfo` state.
    
    The problem was that the `Broker` was trying to connect to the
    `masterAddress` while it was unbind, causing a `Connection Failed`
    message to be returned. But since we were sending manually the
    `Connected` message, we were moving to the `awaitCliqueInfo` anyway.
    
    Even if we can still test the behavior of the actor, I think it's better
    to test with a real TCP connection, to be closer to a real life
    situation. It also ease to test some unhappy path, where we could
    shutdown the TCP connection for example.
    
    Related to: #53

--- flow/src/test/scala/org/alephium/flow/network/bootstrap/BrokerSpec.scala
+++ flow/src/test/scala/org/alephium/flow/network/bootstrap/BrokerSpec.scala
@@ -1,10 +1,11 @@
 package org.alephium.flow.network.bootstrap
 
-import akka.io.Tcp
+import akka.io.{IO, Tcp}
 import akka.testkit.{SocketUtil, TestProbe}
+import akka.util.ByteString
 
 import org.alephium.flow.AlephiumFlowActorSpec
-import org.alephium.protocol.model.ModelGen
+import org.alephium.protocol.model.{BrokerInfo, ModelGen}
 
 class BrokerSpec extends AlephiumFlowActorSpec("BrokerSpec") {
   it should "follow this workflow" in {
@@ -19,19 +20,30 @@ class BrokerSpec extends AlephiumFlowActorSpec("BrokerSpec") {
       Broker.props(masterAddress, config.brokerInfo, config.retryTimeout, bootstrapper.ref))
     watch(broker)
 
-    broker.tell(Tcp.Connected(SocketUtil.temporaryServerAddress(), config.publicAddress),
-                connection.ref)
-    connection.expectMsgType[Tcp.Register]
-    connection.expectMsgType[Tcp.Write]
+    connection.expectMsgPF() {
+      case Tcp.Connected(_, expectedMasterAddress) =>
+        expectedMasterAddress is masterAddress
+    }
+    connection.reply(Tcp.Register(connection.ref))
+
+    connection.expectMsgPF() {
+      case Tcp.Received(data) =>
+        BrokerConnector.deserializeTryWithValidation[BrokerInfo, BrokerInfo.Unsafe](data) is Right(
+          Some((config.brokerInfo, ByteString.empty)))
+    }
 
     val randomInfo = ModelGen.cliqueInfo.sample.get
     val infoData   = BrokerConnector.envelop(randomInfo).data
     broker.tell(Tcp.Received(infoData), connection.ref)
-    connection.expectMsgType[Tcp.Write]
+    connection.expectMsgPF() {
+      case Tcp.Received(data) =>
+        BrokerConnector.deserializeTry[BrokerConnector.Ack](data) is Right(
+          Some((BrokerConnector.Ack(config.brokerInfo.id), ByteString.empty)))
+    }
 
     val ready = BrokerConnector.envelop(CliqueCoordinator.Ready).data
     broker.tell(Tcp.Received(ready), connection.ref)
-    connection.expectMsg(Tcp.ConfirmedClose)
+    connection.expectMsg(Tcp.PeerClosed)
 
     broker ! Tcp.ConfirmedClosed
     bootstrapper.expectMsg(randomInfo)
