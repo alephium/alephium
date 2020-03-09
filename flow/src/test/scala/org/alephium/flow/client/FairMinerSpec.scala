1f0ea50f01151ffdfe35d9b51447a308fbbf0007
Thomas Droxler <thomas.droxler@gmail.com>
2020-03-09 17:24:05 +0100

    Pass `BlockFlow` and `AllHandlers` as direct argument to `FairMiner`
    We were passing the all `Node` class.
    
    As in some previous commit, the goals are to restrict the access of the
    `FaireMiner` to data they shouldn't access and to make this actor more
    easy to Test.
    
    This also remove a lot of warning and unhandled message in the test,
    because we have now the full control of the actor's surrounding.
    
    Related to: #53

--- flow/src/test/scala/org/alephium/flow/client/FairMinerSpec.scala
+++ flow/src/test/scala/org/alephium/flow/client/FairMinerSpec.scala
@@ -1,14 +1,33 @@
 package org.alephium.flow.client
 
+import akka.testkit.TestProbe
+
 import org.alephium.flow.AlephiumFlowActorSpec
-import org.alephium.flow.network.clique.BrokerHandler
+import org.alephium.flow.core.{AllHandlers, BlockFlow, FlowHandler}
+import org.alephium.flow.network.CliqueManager
 
 class FairMinerSpec extends AlephiumFlowActorSpec("FairMiner") {
   it should "initialize FairMiner" in {
-    val builder = new BrokerHandler.Builder {}
-    val node    = Node(builder, "FairMinerSpec")
-    val miner   = system.actorOf(FairMiner.props(node))
+    val cliqueManager        = TestProbe("cliqueManager")
+    val flowHandler          = TestProbe("flowHandler")
+    val blockFlow: BlockFlow = BlockFlow.createUnsafe()
+    val allHandlers: AllHandlers =
+      AllHandlers.buildWithFlowHandler(system, cliqueManager.ref, blockFlow, flowHandler.ref)
+
+    val miner = system.actorOf(FairMiner.props(blockFlow, allHandlers))
+
     miner ! Miner.Start
+
+    cliqueManager.expectMsgType[CliqueManager.BroadCastBlock]
+
+    flowHandler.expectMsgType[FlowHandler.Register]
+
+    flowHandler.expectMsgType[FlowHandler.AddBlock]
+    flowHandler.expectMsgType[FlowHandler.AddBlock]
+    flowHandler.expectMsgType[FlowHandler.AddBlock]
+
     miner ! Miner.Stop
+
+    flowHandler.expectMsgType[FlowHandler.UnRegister.type]
   }
 }
