fc08a4176783cdd4fa06ae248e17787084a72773
Cheng Wang <wachmc@gmail.com>
2020-03-13 01:13:49 +0100

    Refacotr current script system

--- protocol/src/test/scala/org/alephium/protocol/script/ScriptSpec.scala
+++ protocol/src/test/scala/org/alephium/protocol/script/ScriptSpec.scala
@@ -4,10 +4,11 @@ import akka.util.ByteString
 import org.scalatest.EitherValues._
 
 import org.alephium.crypto._
+import org.alephium.protocol.config.ScriptConfig
 import org.alephium.util.{AlephiumSpec, AVector}
 
 class ScriptSpec extends AlephiumSpec {
-  it should "check for public key hash" in {
+  it should "check for public key hash scripts" in {
     forAll { n: Int =>
       val data      = ByteString.fromInts(n)
       val data0     = ByteString.fromInts(n - 1)
@@ -16,18 +17,23 @@ class ScriptSpec extends AlephiumSpec {
       val signature = ED25519.sign(data, sk)
 
       val pubScript = PubScript(
-        AVector[Instruction](OP_KECCAK256, OP_PUSH(pkHash.bytes), OP_EQUALVERIFY, OP_CHECKSIG))
+        AVector[Instruction](OP_DUP,
+                             OP_KECCAK256,
+                             OP_PUSH(pkHash.bytes),
+                             OP_EQUALVERIFY,
+                             OP_CHECKSIG))
       val priScript  = AVector[Instruction](OP_PUSH(pk.bytes))
-      val signatures = AVector(signature.bytes)
+      val signatures = AVector(signature)
       val witness    = Witness(priScript, signatures)
       val witness0   = Witness(priScript, AVector.empty)
       val witness1   = Witness(priScript.init, signatures)
       val witness2   = Witness(OP_PUSH(pk.bytes) +: priScript, signatures)
 
+      implicit val config: ScriptConfig = new ScriptConfig { override def maxStackSize: Int = 100 }
       Script.run(data, pubScript, witness).isRight is true
       Script.run(data0, pubScript, witness).left.value is VerificationFailed
-      Script.run(data, pubScript, witness0).left.value is a[NonCategorized]
-      Script.run(data, pubScript, witness1).left.value is a[NonCategorized]
+      Script.run(data, pubScript, witness0).left.value is InsufficientSignatures
+      Script.run(data, pubScript, witness1).left.value is StackUnderflow
       Script.run(data, pubScript, witness2).left.value is InvalidFinalState
     }
   }
