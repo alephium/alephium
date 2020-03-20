cf3317d845d39b549c94f34c4decf4e3179740fe
Cheng Wang <wachmc@gmail.com>
2020-03-20 16:34:06 +0100

    Implement flow control

--- protocol/src/main/scala/org/alephium/protocol/script/RunFailure.scala
+++ protocol/src/main/scala/org/alephium/protocol/script/RunFailure.scala
@@ -1,14 +1,17 @@
 package org.alephium.protocol.script
 
 sealed trait RunFailure
-case object InvalidFinalState                    extends RunFailure
-case object VerificationFailed                   extends RunFailure
-final case class NonCategorized(message: String) extends RunFailure
-case object StackOverflow                        extends RunFailure
-case object StackUnderflow                       extends RunFailure
-case object IndexOverflow                        extends RunFailure
-case object IndexUnderflow                       extends RunFailure
-case object InsufficientSignatures               extends RunFailure
-case object InvalidPublicKey                     extends RunFailure
-case object IntegerOverFlow                      extends RunFailure
-case class ArithmeticError(message: String)      extends RunFailure
+case object InvalidFinalState                     extends RunFailure
+case object VerificationFailed                    extends RunFailure
+final case class NonCategorized(message: String)  extends RunFailure
+case object StackOverflow                         extends RunFailure
+case object StackUnderflow                        extends RunFailure
+case object IndexOverflow                         extends RunFailure
+case object IndexUnderflow                        extends RunFailure
+case object InsufficientSignatures                extends RunFailure
+case object InvalidPublicKey                      extends RunFailure
+case object InvalidBoolean                        extends RunFailure
+case object IntegerOverFlow                       extends RunFailure
+final case class ArithmeticError(message: String) extends RunFailure
+case object TooManyElses                          extends RunFailure
+case object IncompleteIfScript                    extends RunFailure
--- protocol/src/main/scala/org/alephium/protocol/script/RunState.scala
+++ protocol/src/main/scala/org/alephium/protocol/script/RunState.scala
@@ -1,6 +1,7 @@
 package org.alephium.protocol.script
 
 import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
 
 import akka.util.ByteString
 
@@ -17,10 +18,10 @@ object RunState {
 }
 
 final case class RunState(context: RunContext,
-                          var instructionCount: Int,
+                          var instructionIndex: Int,
                           stack: Stack[ByteString],
                           signatures: Stack[ED25519Signature]) {
-  def currentInstruction: Instruction = context.instructions(instructionCount)
+  def currentInstruction: Instruction = context.instructions(instructionIndex)
 
   @tailrec
   def run(): RunResult[Unit] = {
@@ -33,17 +34,82 @@ final case class RunState(context: RunContext,
     }
   }
 
-  def step(): RunResult[Unit] = {
-    currentInstruction.runWith(this).map { _ =>
-      instructionCount += 1
+  def runIf(condition: Boolean): RunResult[Unit] = {
+    assume(currentInstruction == OP_IF)
+
+    locateElseEndif(instructionIndex).flatMap {
+      case (Some(elseIndex), endifIndex) =>
+        val ifIndex = instructionIndex
+        instructionIndex = endifIndex
+        if (condition) run(context.instructions.slice(ifIndex + 1, elseIndex))
+        else run(context.instructions.slice(elseIndex + 1, endifIndex))
+      case (None, endifIndex) =>
+        val ifIndex = instructionIndex
+        instructionIndex = endifIndex
+        if (condition) run(context.instructions.slice(ifIndex + 1, endifIndex))
+        else Right(())
     }
   }
 
-  def isTerminated: Boolean = instructionCount == context.instructions.length
+  //scalastyle:off return
+  @SuppressWarnings(Array("org.wartremover.warts.While"))
+  def locateElseEndif(currentIndex: Int): RunResult[(Option[Int], Int)] = {
+    import RunStateUtils._
+
+    val track = ArrayBuffer[(IfElseTrack, Int)](If -> currentIndex)
+    var index = currentIndex + 1
+    while (index < context.instructions.length) {
+      context.instructions(index) match {
+        case OP_IF =>
+          track.append(If -> index)
+        case OP_ELSE =>
+          if (track(track.length - 1)._1 == Else) return Left(TooManyElses)
+          track.append(Else -> index)
+        case OP_ENDIF =>
+          track(track.length - 1) match {
+            case (If, _) =>
+              if (track.length == 1) return Right(None -> index)
+              else track.remove(track.length - 1)
+            case (Else, elseIndex) =>
+              if (track.length == 2) {
+                assume(track(0)._1 == If)
+                return Right(Some(elseIndex) -> index)
+              } else {
+                track.remove(track.length - 1)
+                track.remove(track.length - 1)
+              }
+          }
+        case _ => ()
+      }
+      index += 1
+    }
+    Left(IncompleteIfScript)
+  }
+  //scalastyle:on return
+
+  def run(instructions: AVector[Instruction]): RunResult[Unit] = {
+    val newContest = context.copy(instructions = instructions)
+    val newState   = reload(newContest)
+    newState.run()
+  }
+
+  def step(): RunResult[Unit] = {
+    currentInstruction.runWith(this).map { _ =>
+      instructionIndex += 1
+    }
+  }
+
+  def isTerminated: Boolean = instructionIndex == context.instructions.length
 
   def isValidFinalState: Boolean = stack.isEmpty && signatures.isEmpty
 
   def reload(newContext: RunContext): RunState = {
-    this.copy(context = newContext, instructionCount = 0)
+    this.copy(context = newContext, instructionIndex = 0)
   }
+}
+
+case object RunStateUtils {
+  sealed trait IfElseTrack
+  case object If   extends IfElseTrack
+  case object Else extends IfElseTrack
 }
