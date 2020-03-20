6baf475c00b73ec778c8a0ec6c2ea3dd303f6251
Thomas Droxler <thomas.droxler@gmail.com>
2020-03-20 16:21:49 +0100

    Make Bootstrapper fully unit testable
    As the `Bootstrapper` is creating some child actor, we can't have a
    `TestProbe` to write proper unit test.
    
    The solution proposed here, is to have some builders for those child
    actor, like this we can pass a `TestProbe` and write strong unit Test.

--- flow/src/test/scala/org/alephium/flow/network/BootstrapperSpec.scala
+++ flow/src/test/scala/org/alephium/flow/network/BootstrapperSpec.scala
@@ -1,13 +1,50 @@
 package org.alephium.flow.network
 
+import akka.io.Tcp
 import akka.testkit.TestProbe
 
-import org.alephium.flow.AlephiumFlowActorSpec
+import org.alephium.flow._
+import org.alephium.flow.network.bootstrap.InfoFixture
+import org.alephium.protocol.model.ModelGen
 import org.alephium.util.{ActorRefT, AlephiumSpec}
 
 class BootstrapperSpec extends AlephiumSpec {
 
-  it should "bootstrap a master node" in new MasterFixture("BootstrapperSpec-master") {
+  it should "bootstrap a master" in new MasterFixture("BootstrapperSpec-master") {
+    val bootstrapper = system.actorOf(
+      Bootstrapper.props(
+        ActorRefT(serverProbe.ref),
+        ActorRefT(discoveryServerProbe.ref),
+        ActorRefT(cliqueManagerProbe.ref),
+        (_, _) => cliqueCoordinatorProbe.ref,
+        (_, _) => TestProbe().ref
+      ))
+
+    serverProbe.expectMsg(TcpServer.Start(bootstrapper))
+
+    //Peer connects
+    bootstrapper ! connected
+    cliqueCoordinatorProbe.expectMsg(connected)
+
+    //Broker info is full,
+    cliqueManagerProbe.send(bootstrapper, Bootstrapper.ForwardConnection)
+    serverProbe.expectMsg(TcpServer.WorkFor(cliqueManagerProbe.ref))
+
+    //CliqueManager is now responsible for new connection
+    bootstrapper ! connected
+    cliqueManagerProbe.expectMsg(CliqueManager.SendTcpConnected(connected))
+
+    //Receiving IntraCliqueInfo
+    bootstrapper ! Bootstrapper.SendIntraCliqueInfo(intraCliqueInfo)
+    cliqueManagerProbe.expectMsg(CliqueManager.Start(cliqueInfo))
+    discoveryServerProbe.expectMsg(DiscoveryServer.SendCliqueInfo(cliqueInfo))
+
+    //Answering IntraCliqueInfo request
+    bootstrapper ! Bootstrapper.GetIntraCliqueInfo
+    expectMsg(intraCliqueInfo)
+  }
+
+  it should "bootstrap a peer" in new AlephiumFlowActorSpec("BootstrapperSpec") {
     val serverProbe          = TestProbe()
     val discoveryServerProbe = TestProbe()
     val cliqueManagerProbe   = TestProbe()
@@ -18,22 +55,21 @@ class BootstrapperSpec extends AlephiumSpec {
                          ActorRefT(cliqueManagerProbe.ref)))
     serverProbe.expectMsg(TcpServer.Start(bootstrapper))
   }
-  it should "bootstraall a peer node" in new AlephiumFlowActorSpec("BootstrapperSpec") {
-    val serverProbe          = TestProbe()
-    val discoveryServerProbe = TestProbe()
-    val cliqueManagerProbe   = TestProbe()
 
-    val bootstrapper = system.actorOf(
-      Bootstrapper.props(ActorRefT(serverProbe.ref),
-                         ActorRefT(discoveryServerProbe.ref),
-                         ActorRefT(cliqueManagerProbe.ref)))
-    serverProbe.expectMsg(TcpServer.Start(bootstrapper))
-  }
-
-  class MasterFixture(name: String) extends AlephiumFlowActorSpec(name) {
+  class MasterFixture(name: String) extends AlephiumFlowActorSpec(name) with InfoFixture {
     override val configValues = Map(
       ("alephium.network.masterAddress", s"localhost:9972"),
       ("alephium.network.publicAddress", s"localhost:9972")
     )
+
+    val connected =
+      Tcp.Connected(ModelGen.socketAddress.sample.get, ModelGen.socketAddress.sample.get)
+    val intraCliqueInfo = genIntraCliqueInfo
+    val cliqueInfo      = intraCliqueInfo.cliqueInfo
+
+    val serverProbe            = TestProbe()
+    val discoveryServerProbe   = TestProbe()
+    val cliqueManagerProbe     = TestProbe()
+    val cliqueCoordinatorProbe = TestProbe()
   }
 }
