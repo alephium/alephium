55eb396f4a316e0dfeda400ab0c414ef9dc0c78f
Thomas Droxler <thomas.droxler@gmail.com>
2020-03-20 10:30:10 +0100

    Introduce `PlatformConfig`
    This allows to start an `AlephiumFlowActorSpec` with different config
    values, this is necessary for example in `BootstrapperSpec` if we want
    to test the master node case.

--- flow/src/test/scala/org/alephium/flow/network/BootstrapperSpec.scala
+++ flow/src/test/scala/org/alephium/flow/network/BootstrapperSpec.scala
@@ -3,10 +3,11 @@ package org.alephium.flow.network
 import akka.testkit.TestProbe
 
 import org.alephium.flow.AlephiumFlowActorSpec
-import org.alephium.util.ActorRefT
+import org.alephium.util.{ActorRefT, AlephiumSpec}
 
-class BootstrapperSpec extends AlephiumFlowActorSpec("BootstrapperSpec") {
-  it should "bootstrap all actors" in {
+class BootstrapperSpec extends AlephiumSpec {
+
+  it should "bootstrap a master node" in new MasterFixture("BootstrapperSpec-master") {
     val serverProbe          = TestProbe()
     val discoveryServerProbe = TestProbe()
     val cliqueManagerProbe   = TestProbe()
@@ -16,5 +17,23 @@ class BootstrapperSpec extends AlephiumFlowActorSpec("BootstrapperSpec") {
                          ActorRefT(discoveryServerProbe.ref),
                          ActorRefT(cliqueManagerProbe.ref)))
     serverProbe.expectMsg(TcpServer.Start(bootstrapper))
+  }
+  it should "bootstraall a peer node" in new AlephiumFlowActorSpec("BootstrapperSpec") {
+    val serverProbe          = TestProbe()
+    val discoveryServerProbe = TestProbe()
+    val cliqueManagerProbe   = TestProbe()
+
+    val bootstrapper = system.actorOf(
+      Bootstrapper.props(ActorRefT(serverProbe.ref),
+                         ActorRefT(discoveryServerProbe.ref),
+                         ActorRefT(cliqueManagerProbe.ref)))
+    serverProbe.expectMsg(TcpServer.Start(bootstrapper))
+  }
+
+  class MasterFixture(name: String) extends AlephiumFlowActorSpec(name) {
+    override val configValues = Map(
+      ("alephium.network.masterAddress", s"localhost:9972"),
+      ("alephium.network.publicAddress", s"localhost:9972")
+    )
   }
 }
---
+++ flow/src/test/scala/org/alephium/flow/platform/PlatformConfigFixture.scala
@@ -0,0 +1,44 @@
+package org.alephium.flow.platform
+
+import scala.collection.JavaConverters._
+
+import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
+
+import org.alephium.crypto.{ED25519PrivateKey, ED25519PublicKey}
+import org.alephium.flow.io.RocksDBStorage.Settings
+import org.alephium.protocol.config.GroupConfig
+import org.alephium.protocol.model.GroupIndex
+import org.alephium.util.{AVector, Env}
+
+trait PlatformConfigFixture {
+  val configValues: Map[String, Any] = Map.empty
+
+  val genesisBalance: BigInt = 100
+
+  val env      = Env.resolve()
+  val rootPath = Platform.getRootPath(env)
+
+  val newPath = rootPath.resolveSibling(rootPath.getFileName + "-" + this.getClass.getSimpleName)
+
+  lazy val newConfig =
+    ConfigFactory
+      .parseMap(configValues.mapValues(ConfigValueFactory.fromAnyRef(_)).asJava)
+      .withFallback(Configs.parseConfig(newPath))
+
+  lazy val groups0 = newConfig.getInt("alephium.groups")
+
+  lazy val groupConfig = new GroupConfig { override def groups: Int = groups0 }
+
+  lazy val genesisBalances =
+    AVector.tabulate[(ED25519PrivateKey, ED25519PublicKey, BigInt)](groups0) { i =>
+      val groupIndex              = GroupIndex.unsafe(i)(groupConfig)
+      val (privateKey, publicKey) = groupIndex.generateP2pkhKey(groupConfig)
+      (privateKey, publicKey, genesisBalance)
+    }
+
+  implicit lazy val config =
+    PlatformConfig.build(newConfig,
+                         newPath,
+                         Settings.syncWrite,
+                         Some(genesisBalances.map(p => (p._2, p._3))))
+}
