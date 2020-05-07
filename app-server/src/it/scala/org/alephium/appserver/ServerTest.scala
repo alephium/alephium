15ea6f0a68608a0495d79d0314668cdcc40ba8d5
Thomas Droxler <thomas.droxler@gmail.com>
2020-05-07 14:54:12 +0200

    Add Failling clique sync test

--- app-server/src/it/scala/org/alephium/appserver/ServerTest.scala
+++ app-server/src/it/scala/org/alephium/appserver/ServerTest.scala
@@ -66,24 +66,44 @@ class ServerTest extends AlephiumSpec {
   }
 
   it should "boot and sync two cliques" in new Fixture("2-cliques-of-2-nodses") {
+    val fromTs = TimeStamp.now()
+
     val clique1           = bootClique(nbOfNodes = 2)
     val masterPortClique1 = clique1.head.config.masterAddress.getPort
+
+    clique1.foreach(_.start)
+
+    startWS(wsPort(masterPortClique1))
+
+    clique1.foreach { server =>
+      request[Boolean](startMining, rpcPort(server.config.publicAddress.getPort)) is true
+    }
+
+    blockNotifyProbe.receiveN(1, Duration.ofMinutesUnsafe(2).asScala)
+
+    clique1.foreach { server =>
+      request[Boolean](stopMining, rpcPort(server.config.publicAddress.getPort)) is true
+    }
+
+    val selfClique1 = request[SelfClique](getSelfClique, rpcPort(masterPortClique1))
 
     val clique2 =
       bootClique(nbOfNodes = 2,
                  bootstrap = Some(new InetSocketAddress("localhost", masterPortClique1)))
     val masterPortClique2 = clique2.head.config.masterAddress.getPort
 
-    clique1.foreach(_.start)
     clique2.foreach(_.start)
-
-    val selfClique1 = request[SelfClique](getSelfClique, rpcPort(masterPortClique1))
 
     eventually(
       request[Seq[InterCliquePeerInfo]](getInterCliquePeerInfo, rpcPort(masterPortClique2)) is Seq(
         InterCliquePeerInfo(selfClique1.cliqueId,
                             new InetSocketAddress("localhost", masterPortClique1),
                             true)))
+
+    val toTs = TimeStamp.now()
+
+    request[FetchResponse](blockflowFetch(fromTs, toTs), rpcPort(masterPortClique1)).blocks.size is request[
+      FetchResponse](blockflowFetch(fromTs, toTs), rpcPort(masterPortClique2)).blocks.size
   }
 
   it should "work with 2 nodes" in new Fixture("2-nodes") {
@@ -153,11 +173,12 @@ class ServerTest extends AlephiumSpec {
     val defaultMasterPort = SocketUtil.temporaryLocalPort(SocketUtil.Both)
 
     val defaultRpcMasterPort = rpcPort(defaultMasterPort)
-    val defaultWsMasterPort  = defaultMasterPort - 200
+    val defaultWsMasterPort  = wsPort(defaultMasterPort)
 
     def generatePort = SocketUtil.temporaryLocalPort(SocketUtil.Both)
 
     def rpcPort(port: Int) = port - 100
+    def wsPort(port: Int)  = port - 200
 
     val blockNotifyProbe = TestProbe()
 
