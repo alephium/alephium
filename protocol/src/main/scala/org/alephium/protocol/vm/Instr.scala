caa6bbb9725434f8dafc1db7dd555b6d1db12547
Cheng Wang <wachmc@gmail.com>
2020-06-04 22:24:49 +0200

    Add serialization for scripts

--- protocol/src/main/scala/org/alephium/protocol/vm/Instr.scala
+++ protocol/src/main/scala/org/alephium/protocol/vm/Instr.scala
@@ -1,16 +1,109 @@
 package org.alephium.protocol.vm
 
+import scala.collection.immutable.ArraySeq
+
+import akka.util.ByteString
+
+import org.alephium.serde.{Serde, SerdeError, SerdeResult}
 import org.alephium.util
+import org.alephium.util.{Bytes, Collection}
 
 sealed trait Instr[-Ctx <: Context] {
+  def serialize(): ByteString
+
   def runWith[C <: Ctx](frame: Frame[C]): ExeResult[Unit]
 }
+
+object Instr {
+  implicit val statelessSerde: Serde[Instr[StatelessContext]] = new Serde[Instr[StatelessContext]] {
+    override def serialize(input: Instr[StatelessContext]): ByteString = input.serialize()
+
+    override def _deserialize(
+        input: ByteString): SerdeResult[(Instr[StatelessContext], ByteString)] = {
+      for {
+        code <- input.headOption.toRight(SerdeError.notEnoughBytes(1, 0))
+        instrCompanion <- getStatelessCompanion(code).toRight(
+          SerdeError.validation(s"Instruction - invalid code $code"))
+        output <- instrCompanion.deserialize[StatelessContext](input.tail)
+      } yield output
+    }
+  }
+  implicit val statefulSerde: Serde[Instr[StatefulContext]] = new Serde[Instr[StatefulContext]] {
+    override def serialize(input: Instr[StatefulContext]): ByteString = input.serialize()
+
+    override def _deserialize(
+        input: ByteString): SerdeResult[(Instr[StatefulContext], ByteString)] = {
+      for {
+        code <- input.headOption.toRight(SerdeError.notEnoughBytes(1, 0))
+        instrCompanion <- getStatefulCompanion(code).toRight(
+          SerdeError.validation(s"Instruction - invalid code $code"))
+        output <- instrCompanion.deserialize[StatefulContext](input)
+      } yield output
+    }
+  }
+
+  def getStatelessCompanion(byte: Byte): Option[InstrCompanion[StatelessContext]] = {
+    Collection.get(statelessInstrs, Bytes.toPosInt(byte))
+  }
+
+  def getStatefulCompanion(byte: Byte): Option[InstrCompanion[StatefulContext]] = {
+    Collection.get(statefulInstrs, Bytes.toPosInt(byte))
+  }
+
+  // format: off
+  val statelessInstrs: ArraySeq[InstrCompanion[StatelessContext]] = ArraySeq(
+    BoolConstTrue, BoolConstFalse,
+    I32ConstN1, I32Const0, I32Const1, I32Const2, I32Const3, I32Const4, I32Const5,
+                U32Const0, U32Const1, U32Const2, U32Const3, U32Const4, U32Const5,
+    I64ConstN1, I64Const0, I64Const1, I64Const2, I64Const3, I64Const4, I64Const5,
+                U64Const0, U64Const1, U64Const2, U64Const3, U64Const4, U64Const5,
+    I256ConstN1, I256Const0, I256Const1, I256Const2, I256Const3, I256Const4, I256Const5,
+                 U256Const0, U256Const1, U256Const2, U256Const3, U256Const4, U256Const5,
+    LoadLocal, StoreLocal, LoadField, StoreField,
+    Pop, Pop2, Dup, Dup2, Swap,
+    U64Add, U64Sub, U64Mul, U64Div, U64Mod,
+  )
+  val statefulInstrs: ArraySeq[InstrCompanion[StatefulContext]]   = statelessInstrs
+  // format: on
+
+  val toCode: Map[InstrCompanion[StatefulContext], Int] = statefulInstrs.zipWithIndex.toMap
+}
+
 trait StatelessInstr extends Instr[StatelessContext]
 trait StatefulInstr  extends Instr[StatefulContext]
 
+sealed trait InstrCompanion[-Ctx <: Context] {
+  def deserialize[C <: Ctx](input: ByteString): SerdeResult[(Instr[C], ByteString)]
+}
+
+sealed trait InstrCompanion1 extends InstrCompanion[StatelessContext] {
+  lazy val code: Byte = Instr.toCode(this).toByte
+
+  def apply(index: Int): Instr[StatelessContext]
+
+  @inline def from[C <: StatelessContext](byte: Byte): Instr[C] = apply(Bytes.toPosInt(byte))
+
+  override def deserialize[C <: StatelessContext](
+      input: ByteString): SerdeResult[(Instr[C], ByteString)] = {
+    input.headOption
+      .map(byte => (from(byte), input.tail))
+      .toRight(SerdeError.notEnoughBytes(1, 0))
+  }
+}
+
+trait SimpleInstr extends InstrCompanion[StatelessContext] with Instr[StatelessContext] {
+  lazy val code: Byte = Instr.toCode(this).toByte
+
+  override def serialize(): ByteString = ByteString(code)
+
+  override def deserialize[C <: StatelessContext](
+      input: ByteString): SerdeResult[(Instr[C], ByteString)] =
+    Right((this, input))
+}
+
 trait OperandStackInstr extends StatelessInstr
 
-trait ConstInstr extends OperandStackInstr {
+trait ConstInstr extends OperandStackInstr with SimpleInstr {
   def const: Val
 
   override def runWith[C <: StatelessContext](frame: Frame[C]): ExeResult[Unit] = {
@@ -66,7 +159,9 @@ object U256Const3 extends ConstInstr { val const: Val = Val.U256(util.U256.unsaf
 object U256Const4 extends ConstInstr { val const: Val = Val.U256(util.U256.unsafe(4L)) }
 object U256Const5 extends ConstInstr { val const: Val = Val.U256(util.U256.unsafe(5L)) }
 
+// Note: 0 <= index <= 0xFF
 case class LoadLocal(index: Int) extends OperandStackInstr {
+  override def serialize(): ByteString = ByteString(LoadLocal.code, index.toByte)
   override def runWith[C <: StatelessContext](frame: Frame[C]): ExeResult[Unit] = {
     for {
       v <- frame.getLocal(index)
@@ -74,7 +169,9 @@ case class LoadLocal(index: Int) extends OperandStackInstr {
     } yield ()
   }
 }
+object LoadLocal extends InstrCompanion1
 case class StoreLocal(index: Int) extends OperandStackInstr {
+  override def serialize(): ByteString = ByteString(StoreLocal.code, index.toByte)
   override def runWith[C <: StatelessContext](frame: Frame[C]): ExeResult[Unit] = {
     for {
       v <- frame.pop()
@@ -82,7 +179,9 @@ case class StoreLocal(index: Int) extends OperandStackInstr {
     } yield ()
   }
 }
+object StoreLocal extends InstrCompanion1
 case class LoadField(index: Int) extends OperandStackInstr {
+  override def serialize(): ByteString = ByteString(LoadField.code, index.toByte)
   override def runWith[C <: StatelessContext](frame: Frame[C]): ExeResult[Unit] = {
     for {
       v <- frame.getField(index)
@@ -90,7 +189,9 @@ case class LoadField(index: Int) extends OperandStackInstr {
     } yield ()
   }
 }
+object LoadField extends InstrCompanion1
 case class StoreField(index: Int) extends OperandStackInstr {
+  override def serialize(): ByteString = ByteString(StoreField.code, index.toByte)
   override def runWith[C <: StatelessContext](frame: Frame[C]): ExeResult[Unit] = {
     for {
       v <- frame.pop()
@@ -98,34 +199,37 @@ case class StoreField(index: Int) extends OperandStackInstr {
     } yield ()
   }
 }
+object StoreField extends InstrCompanion1
 
-case object Pop extends OperandStackInstr {
+trait PureStackInstr extends OperandStackInstr with SimpleInstr
+
+case object Pop extends PureStackInstr {
   override def runWith[C <: StatelessContext](frame: Frame[C]): ExeResult[Unit] = {
     frame.opStack.remove(1)
   }
 }
-case object Pop2 extends OperandStackInstr {
+case object Pop2 extends PureStackInstr {
   override def runWith[C <: StatelessContext](frame: Frame[C]): ExeResult[Unit] = {
     frame.opStack.remove(2)
   }
 }
-object Dup extends OperandStackInstr {
+object Dup extends PureStackInstr {
   override def runWith[C <: StatelessContext](frame: Frame[C]): ExeResult[Unit] = {
     frame.opStack.dup(1)
   }
 }
-object Dup2 extends OperandStackInstr {
+object Dup2 extends PureStackInstr {
   override def runWith[C <: StatelessContext](frame: Frame[C]): ExeResult[Unit] = {
     frame.opStack.dup(2)
   }
 }
-object Swap extends OperandStackInstr {
+object Swap extends PureStackInstr {
   override def runWith[C <: StatelessContext](frame: Frame[C]): ExeResult[Unit] = {
     frame.opStack.swap(2)
   }
 }
 
-trait ArithmeticInstr extends StatelessInstr
+trait ArithmeticInstr extends SimpleInstr
 
 trait BinaryArithmeticInstr extends ArithmeticInstr {
   protected def op(x: Val, y: Val): ExeResult[Val]
