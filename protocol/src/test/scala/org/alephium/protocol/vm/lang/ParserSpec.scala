c7b1cc76f36853f363b44b8bc3f63509fb0a3843
Cheng Wang <wachmc@gmail.com>
2020-06-08 15:01:10 +0200

    Refine language

--- protocol/src/test/scala/org/alephium/protocol/vm/lang/ParserSpec.scala
+++ protocol/src/test/scala/org/alephium/protocol/vm/lang/ParserSpec.scala
@@ -9,9 +9,10 @@ class ParserSpec extends AlephiumSpec {
   it should "parse lexer" in {
     fastparse.parse("x", Lexer.ident(_)).get.value is Ast.Ident("x")
     fastparse.parse("U64", Lexer.tpe(_)).get.value is Val.U64
-    fastparse.parse("x: U64", Parser.argument(_)).get.value is Ast.Argument(Ast.Ident("x"), Val.U64)
+    fastparse.parse("x: U64", Parser.argument(_)).get.value is
+      Ast.Variable(Ast.Ident("x"), Some(Val.U64), Some(false))
     fastparse
-      .parse("def add(x: U64, y: U64): U64 { return x }\n", Parser.statement(_))
+      .parse("fn add(x: U64, y: U64) -> (U64) { return x }\n", Parser.statement(_))
       .isSuccess is true
   }
 
@@ -28,7 +29,7 @@ class ParserSpec extends AlephiumSpec {
     fastparse.parse("return x + y", Parser.statement(_)).isSuccess is true
     fastparse.parse("return (x + y)", Parser.statement(_)).isSuccess is true
     fastparse
-      .parse("def add(x: U64, y: U64): U64 { return x + y }", Parser.statement(_))
+      .parse("fn add(x: U64, y: U64) -> (U64) { return x + y }", Parser.statement(_))
       .isSuccess is true
   }
 
@@ -38,15 +39,15 @@ class ParserSpec extends AlephiumSpec {
          |var x = 1u
          |var y = 2u
          |
-         |def add(x: U64, y: U64): U64 {
+         |fn add(x: U64, y: U64) -> (U64) {
          |  return (x + y)
          |}
          |
-         |def add(): U64 {
+         |fn add() -> (U64) {
          |  return (x + y)
          |}
          |
-         |def add(d: U64) {
+         |fn add(d: U64) -> () {
          |  var z = d
          |  x = x + z
          |  y = y + z
@@ -65,7 +66,7 @@ class ParserSpec extends AlephiumSpec {
          |var token_box = U64._
          |var alf_box = U64._
          |
-         |def add(alf_amount: U64) -> (U64) {
+         |fn add(alf_amount: U64) -> (U64) {
          |  val token_amount = alf_amount * token_reserve / alf_reserve + 1
          |  val liquidity_mint = alf_amount * total_liquidity / alf_reserve
          |  alf_reserve += alf_amount
@@ -73,6 +74,7 @@ class ParserSpec extends AlephiumSpec {
          |  total_liquidity += liquidity_mint
          |  update(token_box)
          |  update(alf_box)
+         |  return liquidity_mint
          |}
          |""".stripMargin
     contract.nonEmpty is true
