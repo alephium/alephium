51713c2420d0cbc6d554fb4f7236dd70ea3578a2
Cheng Wang <wachmc@gmail.com>
2020-06-11 11:07:18 +0200

    Refactor grammer to make funcdef not statement

--- protocol/src/main/scala/org/alephium/protocol/vm/lang/Parser.scala
+++ protocol/src/main/scala/org/alephium/protocol/vm/lang/Parser.scala
@@ -9,9 +9,10 @@ object Parser {
     P(MultiLineWhitespace.whitespace(ctx) | Lexer.lineComment)
   }
 
-  def const[_: P]: P[Ast.Const]       = P(Lexer.typedNum).map(Ast.Const)
-  def variable[_: P]: P[Ast.Variable] = P(Lexer.ident).map(Ast.Variable)
-  def call[_: P]: P[Ast.Call]         = P(Lexer.ident ~ "(" ~ expr.rep(0, ",") ~ ")").map(Ast.Call.tupled)
+  def const[_: P]: P[Ast.Const]                    = P(Lexer.typedNum).map(Ast.Const)
+  def variable[_: P]: P[Ast.Variable]              = P(Lexer.ident).map(Ast.Variable)
+  def callAbs[_: P]: P[(Ast.Ident, Seq[Ast.Expr])] = P(Lexer.ident ~ "(" ~ expr.rep(0, ",") ~ ")")
+  def call[_: P]: P[Ast.Call]                      = callAbs.map(Ast.Call.tupled)
 
   def binopPrefix[_: P]: P[Ast.Expr]               = P(const | call | variable | parenExpr)
   def binopTerm[_: P]: P[(Ast.Operator, Ast.Expr)] = P(Lexer.operator ~ expr)
@@ -43,8 +44,9 @@ object Parser {
       "fn" ~/ Lexer.ident ~ "(" ~ argument.rep(0, ",") ~ ")" ~ returnType ~
         "{" ~ statement.rep ~ ret ~ "}")
       .map(Ast.FuncDef.tupled)
+  def funcCall[_: P]: P[Ast.FuncCall] = callAbs.map(Ast.FuncCall.tupled)
 
-  def statement[_: P]: P[Ast.Statement] = P(varDef | assign | func)
+  def statement[_: P]: P[Ast.Statement] = P(varDef | assign | funcCall)
 
   def contract[_: P]: P[Ast.Contract] = P(Start ~ varDef.rep ~ func.rep(1)).map(Ast.Contract.tupled)
 }
