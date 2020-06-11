302f75c5bb3bd469c0265f3c8900156be4eef7cf
Cheng Wang <wachmc@gmail.com>
2020-06-11 13:39:56 +0200

    Clean code

--- protocol/src/test/scala/org/alephium/protocol/vm/lang/ParserSpec.scala
+++ protocol/src/test/scala/org/alephium/protocol/vm/lang/ParserSpec.scala
@@ -117,29 +117,6 @@ class ParserSpec extends AlephiumSpec {
     check("mut", "a", "U64", "b", "U64", "U64", "add")
   }
 
-  it should "parse UniSwap" in {
-    val contract =
-      s"""
-         |var alf_reserve = 100u
-         |var token_reserve = 100u
-         |var total_liquidity = 100u
-         |var token_box = U64._
-         |var alf_box = U64._
-         |
-         |fn add(alf_amount: U64) -> (U64) {
-         |  val token_amount = alf_amount * token_reserve / alf_reserve + 1
-         |  val liquidity_mint = alf_amount * total_liquidity / alf_reserve
-         |  alf_reserve += alf_amount
-         |  token_reserve -= token_amount
-         |  total_liquidity += liquidity_mint
-         |  update(token_box)
-         |  update(alf_box)
-         |  return liquidity_mint
-         |}
-         |""".stripMargin
-    contract.nonEmpty is true
-  }
-
   it should "generate IR code" in {
     val input =
       s"""
