bdc20a00d185b114e11216ce63ead468cc6c8e5c
Cheng Wang <wachmc@gmail.com>
2020-07-27 16:55:25 +0200

    Create block validation class

--- flow/src/main/scala/org/alephium/flow/core/validation/BlockValidation.scala
+++ flow/src/main/scala/org/alephium/flow/core/validation/BlockValidation.scala
@@ -3,29 +3,119 @@ package org.alephium.flow.core.validation
 import org.alephium.flow.core.BlockFlow
 import org.alephium.flow.platform.PlatformConfig
 import org.alephium.io.IOResult
-import org.alephium.protocol.model.Block
+import org.alephium.protocol.ALF
+import org.alephium.protocol.model.{Block, TxOutputRef}
 
 object BlockValidation extends Validation[Block, BlockStatus]() {
-  import ValidationStatus.convert
+  import ValidationStatus._
 
   def validate(block: Block, flow: BlockFlow, isSyncing: Boolean)(
       implicit config: PlatformConfig): IOResult[BlockStatus] = {
-    convert(Validation.validateBlock(block, flow, isSyncing), ValidBlock)
+    convert(checkBlock(block, flow, isSyncing), ValidBlock)
   }
 
   def validateUntilDependencies(block: Block,
                                 flow: BlockFlow,
                                 isSyncing: Boolean): IOResult[BlockStatus] = {
-    convert(Validation.validateBlockUntilDependencies(block, flow, isSyncing), ValidBlock)
+    convert(checkBlockUntilDependencies(block, flow, isSyncing), ValidBlock)
   }
 
   def validateAfterDependencies(block: Block, flow: BlockFlow)(
       implicit config: PlatformConfig): IOResult[BlockStatus] = {
-    convert(Validation.validateBlockAfterDependencies(block, flow), ValidBlock)
+    convert(checkBlockAfterDependencies(block, flow), ValidBlock)
   }
 
   def validateAfterHeader(block: Block, flow: BlockFlow)(
       implicit config: PlatformConfig): IOResult[BlockStatus] = {
-    convert(Validation.validateBlockAfterHeader(block, flow), ValidBlock)
+    convert(checkBlockAfterHeader(block, flow), ValidBlock)
+  }
+
+  private[validation] def checkBlockUntilDependencies(
+      block: Block,
+      flow: BlockFlow,
+      isSyncing: Boolean): BlockValidationResult[Unit] = {
+    HeaderValidation.checkHeaderUntilDependencies(block.header, flow, isSyncing)
+  }
+
+  private[validation] def checkBlockAfterDependencies(block: Block, flow: BlockFlow)(
+      implicit config: PlatformConfig): BlockValidationResult[Unit] = {
+    for {
+      _ <- HeaderValidation.checkHeaderAfterDependencies(block.header, flow)
+      _ <- checkBlockAfterHeader(block, flow)
+    } yield ()
+  }
+
+  private[validation] def checkBlock(block: Block, flow: BlockFlow, isSyncing: Boolean)(
+      implicit config: PlatformConfig): BlockValidationResult[Unit] = {
+    for {
+      _ <- HeaderValidation.checkHeader(block.header, flow, isSyncing)
+      _ <- checkBlockAfterHeader(block, flow)
+    } yield ()
+  }
+
+  private[validation] def checkBlockAfterHeader(block: Block, flow: BlockFlow)(
+      implicit config: PlatformConfig): BlockValidationResult[Unit] = {
+    for {
+      _ <- checkGroup(block)
+      _ <- checkNonEmptyTransactions(block)
+      _ <- checkCoinbase(block)
+      _ <- checkMerkleRoot(block)
+      _ <- checkNonCoinbases(block, flow)
+    } yield ()
+  }
+
+  private[validation] def checkGroup(block: Block)(
+      implicit config: PlatformConfig): BlockValidationResult[Unit] = {
+    if (block.chainIndex.relateTo(config.brokerInfo)) validBlock(())
+    else invalidBlock(InvalidGroup)
+  }
+
+  private[validation] def checkNonEmptyTransactions(block: Block): BlockValidationResult[Unit] = {
+    if (block.transactions.nonEmpty) validBlock(()) else invalidBlock(EmptyTransactionList)
+  }
+
+  private[validation] def checkCoinbase(block: Block): BlockValidationResult[Unit] = {
+    val coinbase = block.coinbase // Note: validateNonEmptyTransactions first pls!
+    val unsigned = coinbase.unsigned
+    if (unsigned.inputs.isEmpty && unsigned.fixedOutputs.length == 1 && coinbase.generatedOutputs.isEmpty && coinbase.signatures.isEmpty)
+      validBlock(())
+    else invalidBlock(InvalidCoinbase)
+  }
+
+  // TODO: use Merkle hash for transactions
+  private[validation] def checkMerkleRoot(block: Block): BlockValidationResult[Unit] = {
+    if (block.header.txsHash == ALF.Hash.hash(block.transactions)) validBlock(())
+    else invalidBlock(InvalidMerkleRoot)
+  }
+
+  private[validation] def checkNonCoinbases(block: Block, flow: BlockFlow)(
+      implicit config: PlatformConfig): BlockValidationResult[Unit] = {
+    val index      = block.chainIndex
+    val brokerInfo = config.brokerInfo
+    assert(index.relateTo(brokerInfo))
+
+    if (brokerInfo.contains(index.from)) {
+      val result = for {
+        _    <- checkBlockDoubleSpending(block)
+        trie <- ValidationStatus.from(flow.getPersistedTrie(block))
+        _    <- block.nonCoinbase.foreachE(NonCoinbaseValidation.checkBlockTx(_, trie))
+      } yield ()
+      convert(result)
+    } else {
+      validBlock(())
+    }
+  }
+
+  private[validation] def checkBlockDoubleSpending(block: Block): TxValidationResult[Unit] = {
+    val utxoUsed = scala.collection.mutable.Set.empty[TxOutputRef]
+    block.nonCoinbase.foreachE { tx =>
+      tx.unsigned.inputs.foreachE { input =>
+        if (utxoUsed.contains(input.outputRef)) invalidTx(DoubleSpending)
+        else {
+          utxoUsed += input.outputRef
+          validTx(())
+        }
+      }
+    }
   }
 }
--- flow/src/main/scala/org/alephium/flow/core/validation/Validation.scala
+++ flow/src/main/scala/org/alephium/flow/core/validation/Validation.scala
@@ -6,7 +6,6 @@ import org.alephium.io.IOResult
 import org.alephium.protocol.ALF.Hash
 import org.alephium.protocol.config.GroupConfig
 import org.alephium.protocol.model._
-import org.alephium.serde._
 import org.alephium.util.{AVector, Forest}
 
 abstract class Validation[T <: FlowData, S <: ValidationStatus]() {
@@ -21,105 +20,6 @@ abstract class Validation[T <: FlowData, S <: ValidationStatus]() {
 
 // scalastyle:off number.of.methods
 object Validation {
-  import ValidationStatus._
-
-  private[validation] def validateBlockUntilDependencies(
-      block: Block,
-      flow: BlockFlow,
-      isSyncing: Boolean): BlockValidationResult[Unit] = {
-    HeaderValidation.checkHeaderUntilDependencies(block.header, flow, isSyncing)
-  }
-
-  private[validation] def validateBlockAfterDependencies(block: Block, flow: BlockFlow)(
-      implicit config: PlatformConfig): BlockValidationResult[Unit] = {
-    for {
-      _ <- HeaderValidation.checkHeaderAfterDependencies(block.header, flow)
-      _ <- validateBlockAfterHeader(block, flow)
-    } yield ()
-  }
-
-  private[validation] def validateBlock(block: Block, flow: BlockFlow, isSyncing: Boolean)(
-      implicit config: PlatformConfig): BlockValidationResult[Unit] = {
-    for {
-      _ <- HeaderValidation.checkHeader(block.header, flow, isSyncing)
-      _ <- validateBlockAfterHeader(block, flow)
-    } yield ()
-  }
-
-  private[validation] def validateBlockAfterHeader(block: Block, flow: BlockFlow)(
-      implicit config: PlatformConfig): BlockValidationResult[Unit] = {
-    for {
-      _ <- checkGroup(block)
-      _ <- checkNonEmptyTransactions(block)
-      _ <- checkCoinbase(block)
-      _ <- checkMerkleRoot(block)
-      _ <- checkNonCoinbases(block, flow)
-    } yield ()
-  }
-
-  /*
-   * The following functions are all the check functions behind validations
-   */
-
-  private[validation] def checkGroup(block: Block)(
-      implicit config: PlatformConfig): BlockValidationResult[Unit] = {
-    if (block.chainIndex.relateTo(config.brokerInfo)) validBlock(())
-    else invalidBlock(InvalidGroup)
-  }
-
-  private[validation] def checkNonEmptyTransactions(block: Block): BlockValidationResult[Unit] = {
-    if (block.transactions.nonEmpty) validBlock(()) else invalidBlock(EmptyTransactionList)
-  }
-
-  private[validation] def checkCoinbase(block: Block): BlockValidationResult[Unit] = {
-    val coinbase = block.coinbase // Note: validateNonEmptyTransactions first pls!
-    val unsigned = coinbase.unsigned
-    if (unsigned.inputs.isEmpty && unsigned.fixedOutputs.length == 1 && coinbase.generatedOutputs.isEmpty && coinbase.signatures.isEmpty)
-      validBlock(())
-    else invalidBlock(InvalidCoinbase)
-  }
-
-  // TODO: use Merkle hash for transactions
-  private[validation] def checkMerkleRoot(block: Block): BlockValidationResult[Unit] = {
-    if (block.header.txsHash == Hash.hash(block.transactions)) validBlock(())
-    else invalidBlock(InvalidMerkleRoot)
-  }
-
-  private[validation] def checkNonCoinbases(block: Block, flow: BlockFlow)(
-      implicit config: PlatformConfig): BlockValidationResult[Unit] = {
-    val index      = block.chainIndex
-    val brokerInfo = config.brokerInfo
-    assert(index.relateTo(brokerInfo))
-
-    if (brokerInfo.contains(index.from)) {
-      val result = for {
-        _    <- checkBlockDoubleSpending(block)
-        trie <- ValidationStatus.from(flow.getPersistedTrie(block))
-        _    <- block.nonCoinbase.foreachE(NonCoinbaseValidation.checkBlockTx(_, trie))
-      } yield ()
-      convert(result)
-    } else {
-      validBlock(())
-    }
-  }
-
-  private[validation] def checkBlockDoubleSpending(block: Block): TxValidationResult[Unit] = {
-    val utxoUsed = scala.collection.mutable.Set.empty[TxOutputRef]
-    block.nonCoinbase.foreachE { tx =>
-      tx.unsigned.inputs.foreachE { input =>
-        if (utxoUsed.contains(input.outputRef)) invalidTx(DoubleSpending)
-        else {
-          utxoUsed += input.outputRef
-          validTx(())
-        }
-      }
-    }
-  }
-
-  /*
-   * The following functions are for other type of validation
-   */
-
   @SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
   def validateFlowDAG[T <: FlowData](datas: AVector[T])(
       implicit config: GroupConfig): Option[AVector[Forest[Hash, T]]] = {
