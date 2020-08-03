d625f52152382c99b23c0e3c71fd3c0ea3bc8e6e
Thomas Droxler <thomas.droxler@gmail.com>
2020-08-03 17:43:21 +0200

    Add an implicit `ConfigReader[ArraySeq[InetSocketAddress]]`
    It first try to automatically parse a list and if it fails it fall back
    on parsing a string representing a list: "str1,str2"
    
    Without this we can't pass the `bootstrap` value as an environment
    variable, as it's interpreted as a `String`.

--- flow/src/test/scala/org/alephium/flow/setting/AlephiumConfigSpec.scala
+++ flow/src/test/scala/org/alephium/flow/setting/AlephiumConfigSpec.scala
@@ -1,16 +1,42 @@
 package org.alephium.flow.setting
 
 import java.io.File
+import java.net.InetSocketAddress
+
+import scala.collection.immutable.ArraySeq
 
 import com.typesafe.config.ConfigFactory
+import pureconfig.ConfigSource
+import pureconfig.generic.auto._
 
 import org.alephium.util.AlephiumSpec
 
 class AlephiumConfigSpec extends AlephiumSpec {
-  it should "load api config" in {
+  it should "load alephium config" in {
     val path   = getClass.getResource(s"/system_test.conf.tmpl").getPath
     val file   = new File(path)
     val config = ConfigFactory.parseFile(file)
     AlephiumConfig.load(config.getConfig("alephium")).isRight is true
+  }
+
+  it should "load bootstrap config" in {
+    import PureConfigUtils._
+
+    case class Bootstrap(addresses: ArraySeq[InetSocketAddress])
+
+    val expected = Bootstrap(
+      ArraySeq(new InetSocketAddress("localhost", 1234), new InetSocketAddress("localhost", 4321)))
+
+    ConfigSource
+      .string("""{ addresses = ["localhost:1234", "localhost:4321"] }""")
+      .load[Bootstrap] isE expected
+
+    ConfigSource
+      .string("""{ addresses = "localhost:1234,localhost:4321" }""")
+      .load[Bootstrap] isE expected
+
+    ConfigSource
+      .string("""{ addresses = "" }""")
+      .load[Bootstrap] isE Bootstrap(ArraySeq.empty)
   }
 }
