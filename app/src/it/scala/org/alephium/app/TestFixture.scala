72e9130a0d52507db624d1d0eec413e95fe3c629
Cheng Wang <wachmc@gmail.com>
2020-10-09 01:24:34 +0200

    Update test addresses

--- app/src/it/scala/org/alephium/app/TestFixture.scala
+++ app/src/it/scala/org/alephium/app/TestFixture.scala
@@ -71,9 +71,9 @@ trait TestFixtureLike
     (Address.p2pkh(networkType, pubKey).toBase58, pubKey.toHexString, priKey.toHexString)
   }
 
-  val address    = "T1CarrVGRS2YvEoPLgY6HJvyCAcqW91buYWRJ4roRDCPPg"
-  val publicKey  = "033cd0876b492fea9c5c61f616ca3faf006097959c67f663ce7653ecb0b71b4eb4"
-  val privateKey = "bf38aa6f383b14e678a5fd885b93dd97d1add94aaddb70b7cfb6e4c5e8330fe9"
+  val address    = "T1D9PBcRXK5uzrNYokNMB7oh6JpW86sZajJ5gD845cshED"
+  val publicKey  = "03ee6c463816f1b7f3d23ccff248fa73d114ac5eabd616b1fd0d35e6f5ec1eb092"
+  val privateKey = "a13841774e62ef76b4d9aac1531b68a46e93baf09ba9c692d57a0edc64b973d4"
   val mnemonic =
     "talent box celery scissors april bone pet broken borrow weather laugh property rose disorder radar saddle enemy toddler olive dance sudden idle hazard tuition"
   val (transferAddress, _, _) = generateAccount
---
+++ wallet/src/main/scala/org/alephium/wallet/WalletGen.scala
@@ -0,0 +1,64 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.wallet
+
+import scala.annotation.tailrec
+
+import org.alephium.crypto.{SecP256K1PrivateKey, SecP256K1PublicKey}
+import org.alephium.crypto.wallet.{BIP32, Mnemonic}
+import org.alephium.protocol.config.GroupConfig
+import org.alephium.protocol.model.{Address, GroupIndex, NetworkType}
+
+@SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
+object WalletGen extends App {
+  @tailrec
+  def gen(networkType: NetworkType, groupIndex: GroupIndex)(implicit config: GroupConfig)
+    : (Address, SecP256K1PublicKey, SecP256K1PrivateKey, Mnemonic) = {
+    // scalastyle:off magic.number
+    val mnemonic = Mnemonic.generate(24).get
+    // scalastyle:on magic.number
+
+    val seed        = mnemonic.toSeed("")
+    val extendedKey = BIP32.btcMasterKey(seed).derive(Constants.path(networkType)).get
+    val priKey      = extendedKey.privateKey
+    val pubKey      = extendedKey.publicKey
+    val address     = Address.p2pkh(networkType, pubKey)
+    if (address.groupIndex == groupIndex) {
+      (address, pubKey, priKey, mnemonic)
+    } else {
+      gen(networkType, groupIndex)
+    }
+  }
+
+  // scalastyle:off regex
+  Seq[(NetworkType, Int)](NetworkType.Testnet -> 4, NetworkType.Devnet -> 3).foreach {
+    case (networkType, groupNum) =>
+      println(networkType.name)
+      implicit val config: GroupConfig = new GroupConfig {
+        override def groups: Int = groupNum
+      }
+      (0 until groupNum).foreach { g =>
+        println(s"group: $g")
+        val (address, pubKey, priKey, mnemonic) = gen(networkType, GroupIndex.unsafe(g))
+        println(s"address: ${address.toBase58}")
+        println(s"pubKey: ${pubKey.toHexString}")
+        println(s"priKey: ${priKey.toHexString}")
+        println(s"mnemonic: ${mnemonic.toLongString}")
+      }
+      println()
+  }
+}
