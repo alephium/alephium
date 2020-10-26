1fe668ef8609dec38ce9b557bc04c361002c0f46
Cheng Wang <wachmc@gmail.com>
2020-10-26 15:41:29 +0100

    Introduce Gas

---
+++ protocol/src/main/scala/org/alephium/protocol/vm/GasSchedule.scala
@@ -0,0 +1,75 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.protocol.vm
+
+sealed trait GasSchedule
+
+sealed trait GasSimple extends GasSchedule {
+  def gas: Int
+}
+
+sealed trait GasFormula extends GasSchedule {
+  def gas(count: Int): Int
+}
+
+trait GasZero extends GasSimple {
+  val gas: Int = 0
+}
+
+trait GasVeryLow extends GasSimple {
+  val gas: Int = 3
+}
+
+trait GasLow extends GasSimple {
+  val gas: Int = 5
+}
+
+trait GasMid extends GasSimple {
+  val gas: Int = 8
+}
+
+trait GasHigh extends GasSimple {
+  val gas: Int = 10
+}
+
+trait GasHash extends GasFormula {
+  def gas(inputLength: Int): Int = (30 + 6 * ((inputLength + 3) / 8))
+}
+
+trait GasSignature extends GasSimple {
+  val gas: Int = 500 // TODO: bench this
+}
+
+trait GasCreate extends GasSimple {
+  val gas: Int = 32000
+}
+
+trait GasBalance extends GasSimple {
+  val gas: Int = 50
+}
+
+trait GasCall extends GasFormula {
+  def gas(count: Int): Int = ??? // should call the companion object instead
+}
+
+object GasCall {
+  def stateless(): Int = 200
+
+  def statefulLocal(): Int = 200
+
+  def statefulExternal(): Int = 5000
+}
