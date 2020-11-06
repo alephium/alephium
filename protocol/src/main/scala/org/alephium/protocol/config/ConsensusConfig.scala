45224e28119b34943593ca2285a83e90c5de790c
Cheng Wang <wachmc@gmail.com>
2020-11-06 16:19:46 +0100

    Remove block time from config

--- protocol/src/main/scala/org/alephium/protocol/config/ConsensusConfig.scala
+++ protocol/src/main/scala/org/alephium/protocol/config/ConsensusConfig.scala
@@ -16,6 +16,7 @@
 
 package org.alephium.protocol.config
 
+import org.alephium.protocol.mining.Emission
 import org.alephium.protocol.model.Target
 import org.alephium.util.Duration
 
@@ -24,11 +25,10 @@ trait ConsensusConfig {
   def numZerosAtLeastInHash: Int
   def maxMiningTarget: Target
 
-  def blockTargetTime: Duration
   // scalastyle:off magic.number
-  def maxHeaderTimeStampDrift: Duration = blockTargetTime.timesUnsafe(20)
+  def maxHeaderTimeStampDrift: Duration = Emission.blockTargetTime.timesUnsafe(20)
   // scalastyle:on magic.number
 
   def tipsPruneInterval: Int
-  def tipsPruneDuration: Duration = blockTargetTime.timesUnsafe(tipsPruneInterval.toLong)
+  def tipsPruneDuration: Duration = Emission.blockTargetTime.timesUnsafe(tipsPruneInterval.toLong)
 }
