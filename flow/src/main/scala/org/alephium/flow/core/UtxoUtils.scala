b484ce5d2c21cc897cfb1ff2cdbbb10dc951df20
Thomas Droxler <thomas.droxler@gmail.com>
2021-03-18 11:36:33 +0100

    Introduce `UtxoUtils`

---
+++ flow/src/main/scala/org/alephium/flow/core/UtxoUtils.scala
@@ -0,0 +1,117 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.flow.core
+
+import scala.annotation.tailrec
+
+import org.alephium.protocol.model._
+import org.alephium.util._
+
+/*
+ * We first travel all utxos and organize them in the state
+ *   - if the amout match the target we take it and stop the iteration
+ *   - if it smaller we keep the value in our `smallers` list
+ *   - if it's bigger, we keep it if it's the smallest bigger
+ *
+ *  With the `State` we have everything to select the rights utxos
+ *  - If we have a matched value, we use only that one
+ *  - If the smallers sum match the target we use all of them (for cleaning
+ *    and also because it might be a "wallet dump"
+ *  - If the smallers sum is less than the target we use the smallest greater (if exist)
+ *  - If the smallers sum is bigger than the target, we use them, but clean to keep the
+ *    minimum set of utxos (might be changed to use the random combinaison of bitcoin
+ *    to avoid the sort)
+ *  - Otherwise we return None as we can't reach the target
+ */
+object UtxoUtils {
+
+  type Asset = (AssetOutputRef, AssetOutput)
+
+  private final case class State(smallers: AVector[Asset],
+                                 smallestGreater: Option[Asset],
+                                 matching: Option[Asset])
+
+  private def sumAssets(assets: AVector[Asset]): U256 = {
+    assets.fold(U256.Zero) { (total, asset) =>
+      total.addUnsafe(asset._2.amount)
+    }
+  }
+
+  def select(utxos: AVector[Asset], target: U256): Option[AVector[Asset]] = {
+    val state = buildState(utxos, target)
+
+    val smallersSum = sumAssets(state.smallers)
+
+    state match {
+      case State(_, _, Some(matching)) =>
+        Some(AVector(matching))
+      case State(smallers, _, _) if smallersSum == target =>
+        Some(smallers)
+      case State(_, Some(smallestGreater), _) if smallersSum < target =>
+        Some(AVector(smallestGreater))
+      case State(smallers, _, _) if smallersSum > target =>
+        Some(reduceSmallerValue(smallers, smallersSum, target))
+      case State(_, Some(smallestGreater), _) => Some(AVector(smallestGreater))
+      case _                                  => None
+    }
+  }
+
+  private def buildState(initialAssets: AVector[Asset], target: U256): State = {
+    @tailrec
+    def iter(state: State, assets: AVector[Asset]): State = {
+      assets.headOption match {
+        case None => state
+        case Some(asset) =>
+          val amount = asset._2.amount
+          if (amount == target) {
+            state.copy(matching = Some(asset))
+          } else if (amount < target) {
+            iter(state.copy(smallers = state.smallers :+ asset), assets.tail)
+          } else {
+            state.smallestGreater match {
+              case Some(currentSmallerGreater) if amount >= currentSmallerGreater._2.amount =>
+                iter(state, assets.tail)
+              case _ =>
+                iter(state.copy(smallestGreater = Some(asset)), assets.tail)
+            }
+          }
+      }
+    }
+
+    iter(State(AVector.empty, None, None), initialAssets)
+  }
+
+  private def reduceSmallerValue(assets: AVector[Asset],
+                                 currentValue: U256,
+                                 target: U256): AVector[Asset] = {
+    @tailrec
+    def iter(sorted: AVector[Asset], value: U256): AVector[Asset] = {
+      sorted.headOption.flatMap(asset => value.sub(asset._2.amount)) match {
+        case Some(newValue) =>
+          if (newValue >= target) {
+            iter(sorted.tail, newValue)
+          } else {
+            sorted
+          }
+        case None => sorted
+      }
+    }
+
+    assume(currentValue > target)
+    iter(assets.sortBy(_._2.amount), currentValue)
+  }
+}
---
+++ flow/src/test/scala/org/alephium/flow/core/UtxoUtilsSpec.scala
@@ -0,0 +1,89 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.flow.core
+
+import akka.util.ByteString
+
+import org.alephium.flow.setting.AlephiumConfigFixture
+import org.alephium.protocol.Hash
+import org.alephium.protocol.config.GroupConfig
+import org.alephium.protocol.model._
+import org.alephium.protocol.vm.LockupScript
+import org.alephium.util._
+
+// scalastyle:off number.of.methods
+class UtxoUtilsSpec extends AlephiumSpec with LockupScriptGenerators {
+
+  implicit val groupConfig = new GroupConfig {
+    override def groups: Int = 2
+  }
+
+  it should "return the matching utxo when exists" in new Fixture {
+    val utxos = buildUtxos(2, 1, 3)
+
+    UtxoUtils.select(utxos, U256.unsafe(1)) is Some(AVector(utxos(1)))
+  }
+
+  it should "return everything in case of wallet dump" in new Fixture {
+    val utxos = buildUtxos(1, 2, 3)
+
+    UtxoUtils.select(utxos, U256.unsafe(6)) is Some(AVector(utxos(0), utxos(1), utxos(2)))
+  }
+
+  it should "return all the smallers value if their sum match the target" in new Fixture {
+    val utxos = buildUtxos(1, 2, 3, 7, 8, 9)
+
+    UtxoUtils.select(utxos, U256.unsafe(6)) is Some(AVector(utxos(0), utxos(1), utxos(2)))
+  }
+
+  it should "return the smallest greater if the sum of the smallest is less than the target" in new Fixture {
+    val utxos = buildUtxos(1, 2, 3, 8, 9)
+
+    UtxoUtils.select(utxos, U256.unsafe(7)) is Some(AVector(utxos(3)))
+  }
+
+  it should "return None if not enough amount" in new Fixture {
+    val utxos = buildUtxos(1, 2, 3)
+
+    UtxoUtils.select(utxos, U256.unsafe(7)) is None
+  }
+
+  it should "return the minimum set of smallers value if their sum is bigger than the match" in new Fixture {
+    val utxos = buildUtxos(1, 2, 3, 7, 8, 9)
+
+    UtxoUtils.select(utxos, U256.unsafe(5)) is Some(AVector(utxos(1), utxos(2)))
+  }
+
+  trait Fixture extends AlephiumConfigFixture {
+
+    def buildOutput(lockupScript: LockupScript, amount: U256): (AssetOutputRef, AssetOutput) = {
+      val output =
+        AssetOutput(amount, lockupScript, TimeStamp.now(), AVector.empty, ByteString.empty)
+      val ref = AssetOutputRef.unsafe(Hint.from(output), Hash.generate)
+      (ref, output)
+    }
+
+    val defaultLockupScript = p2pkhLockupGen(GroupIndex.unsafe(0)).sample.get
+
+    def buildUtxos(amounts: Int*): AVector[(AssetOutputRef, AssetOutput)] = {
+      AVector.from(amounts.map { amount =>
+        buildOutput(defaultLockupScript, U256.unsafe(amount))
+      })
+    }
+
+  }
+}
