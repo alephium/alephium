75876f18b35b91380528898c84d50822693c000e
Cheng Wang <wachmc@gmail.com>
2021-06-25 02:24:14 +0200

    Update tips utils

--- flow/src/main/scala/org/alephium/flow/core/FlowUtils.scala
+++ flow/src/main/scala/org/alephium/flow/core/FlowUtils.scala
@@ -93,7 +93,7 @@ trait FlowUtils
         }
       case Reorg(toRemove, toAdd) =>
         val (removed, added) = getMemPool(mainGroup).reorg(toRemove, toAdd)
-        logger.debug(s"Reorg for #$mainGroup mempool: #$removed removed, #$added added")
+        logger.info(s"Reorg for #$mainGroup mempool: #$removed removed, #$added added")
     }
   }
 
