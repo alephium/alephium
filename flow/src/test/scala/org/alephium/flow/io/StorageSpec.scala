ac3082ec877e8f10c0e87f3d64b436f776a23ccc
lbqds <lbqds@outlook.com>
2021-07-26 09:45:22 +0800

    Add StorageSpec

--- flow/src/test/scala/org/alephium/flow/io/StorageSpec.scala
+++ flow/src/test/scala/org/alephium/flow/io/StorageSpec.scala
@@ -16,35 +16,31 @@
 
 package org.alephium.flow.io
 
-import java.nio.file.{Files, Path}
+import org.scalatest.{BeforeAndAfterEach, Suite}
 
 import org.alephium.io.RocksDBSource
-import org.alephium.protocol.Hash
-import org.alephium.protocol.config.GroupConfig
+import org.alephium.protocol.config.ConsensusConfigFixture
+import org.alephium.util.{AlephiumSpec, Files}
 
-trait StoragesFixture {
-  def storages: Storages
+trait StorageSpec[S]
+    extends ConsensusConfigFixture.Default
+    with AlephiumSpec
+    with BeforeAndAfterEach { self: Suite =>
+  val dbname: String
+  val builder: RocksDBSource => S
+  lazy val dbPath           = Files.tmpDir.resolve(dbname)
+  var source: RocksDBSource = _
+  var storage: S            = _
 
-  def cleanStorages(): Unit = {
-    storages.dESTROYUnsafe()
-  }
-}
-
-object StoragesFixture {
-  def buildStorages(rootPath: Path)(implicit groupConfig: GroupConfig): Storages = {
-    if (!Files.exists(rootPath)) rootPath.toFile.mkdir()
-
-    val postFix   = Hash.random.toHexString
-    val dbFolders = s"db-$postFix"
-    val storages: Storages =
-      Storages.createUnsafe(rootPath, dbFolders, RocksDBSource.Settings.syncWrite)
-    storages
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    source = RocksDBSource.openUnsafe(dbPath, RocksDBSource.Compaction.HDD)
+    storage = builder(source)
   }
 
-  trait Default extends StoragesFixture {
-    def rootPath: Path
-    implicit def groupConfig: GroupConfig
-
-    lazy val storages = StoragesFixture.buildStorages(rootPath)
+  override def afterEach(): Unit = {
+    super.afterEach()
+    source.dESTROY().rightValue
+    ()
   }
 }
