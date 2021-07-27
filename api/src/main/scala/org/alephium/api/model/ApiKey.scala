8e2fc0732ba5e72bd55bc4fde2ab512707320731
Thomas Droxler <thomas.droxler@gmail.com>
2021-07-27 09:30:22 +0200

    Introduce `ApiKey` model

---
+++ api/src/main/scala/org/alephium/api/model/ApiKey.scala
@@ -0,0 +1,31 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.api.model
+
+final case class ApiKey private (val value: String)
+
+object ApiKey {
+  def unsafe(raw: String): ApiKey = new ApiKey(raw)
+
+  def from(raw: String): Either[String, ApiKey] = {
+    if (raw.length < 32) {
+      Left("Api key must have at least 32 characters")
+    } else {
+      Right(new ApiKey(raw))
+    }
+  }
+}
