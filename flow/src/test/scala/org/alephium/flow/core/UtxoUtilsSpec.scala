066dcd5f392e6c0670eb310c6721b239808337ec
Hongchao Liu <lbs.lhc@gmail.com>
2021-08-11 18:15:02 +0200

    Simplify test code for UTXO selection

--- flow/src/test/scala/org/alephium/flow/core/UtxoUtilsSpec.scala
+++ flow/src/test/scala/org/alephium/flow/core/UtxoUtilsSpec.scala
@@ -17,6 +17,7 @@
 package org.alephium.flow.core
 
 import akka.util.ByteString
+import org.scalatest.compatible.Assertion
 
 import org.alephium.flow.core.FlowUtils.{AssetOutputInfo, PersistedOutput, UnpersistedBlockOutput}
 import org.alephium.flow.core.UtxoUtils.Selected
@@ -35,20 +36,20 @@ class UtxoUtilsSpec extends AlephiumSpec with LockupScriptGenerators {
   }
 
   it should "return the utxos with the amount from small to large, without tokens" in new Fixture {
-    val utxos = buildUtxos(20, 10, 30)
+    implicit val utxos = buildUtxos(20, 10, 30)
 
-    selectWithoutGas(utxos, 10) isE AVector(utxos(1))
-    selectWithoutGas(utxos, 20) isE AVector(utxos(1), utxos(0))
-    selectWithoutGas(utxos, 30) isE AVector(utxos(1), utxos(0))
-    selectWithoutGas(utxos, 40) isE AVector(utxos(1), utxos(0), utxos(2))
-    selectWithoutGas(utxos, 50) isE AVector(utxos(1), utxos(0), utxos(2))
-    selectWithoutGas(utxos, 60) isE AVector(utxos(1), utxos(0), utxos(2))
-    selectWithoutGas(utxos, 70).leftValue.startsWith(s"Not enough balance") is true
+    UtxoSelection(10).verify(1)
+    UtxoSelection(20).verify(1, 0)
+    UtxoSelection(30).verify(1, 0)
+    UtxoSelection(40).verify(1, 0, 2)
+    UtxoSelection(50).verify(1, 0, 2)
+    UtxoSelection(60).verify(1, 0, 2)
+    UtxoSelection(70).leftValue.startsWith(s"Not enough balance") is true
 
-    selectWithoutGas(utxos, 29, dustAmount = 1) isE AVector(utxos(1), utxos(0))
-    selectWithoutGas(utxos, 29, dustAmount = 2) isE AVector(utxos(1), utxos(0), utxos(2))
-    selectWithoutGas(utxos, 30, dustAmount = 1) isE AVector(utxos(1), utxos(0))
-    selectWithoutGas(utxos, 59, dustAmount = 2).leftValue.startsWith(s"Not enough balance") is true
+    UtxoSelection(29).withDust(1).verify(1, 0)
+    UtxoSelection(29).withDust(2).verify(1, 0, 2)
+    UtxoSelection(30).withDust(1).verify(1, 0)
+    UtxoSelection(59).withDust(2).leftValue.startsWith(s"Not enough balance") is true
   }
 
   it should "return the utxos with the amount from small to large, with tokens" in new Fixture {
@@ -56,60 +57,34 @@ class UtxoUtilsSpec extends AlephiumSpec with LockupScriptGenerators {
     val tokenId2 = Hash.hash("tokenId2")
     val tokenId3 = Hash.hash("tokenId3")
 
-    val utxos = buildUtxosWithTokens(
+    implicit val utxos = buildUtxosWithTokens(
       (20, AVector((tokenId1, 10), (tokenId2, 20))),
       (10, AVector.empty),
       (30, AVector((tokenId1, 2), (tokenId3, 10)))
     )
 
-    selectWithoutGas(utxos, 10) isE AVector(utxos(1))
-    selectWithoutGas(utxos, 20) isE AVector(utxos(1), utxos(0))
-    selectWithoutGas(utxos, 20, AVector((tokenId1, 1))) isE AVector(utxos(1), utxos(0))
-    selectWithoutGas(utxos, 20, AVector((tokenId3, 5))) isE AVector(utxos(1), utxos(0), utxos(2))
-    selectWithoutGas(utxos, 20, AVector((tokenId1, 10))) isE AVector(utxos(1), utxos(0))
-    selectWithoutGas(utxos, 20, AVector((tokenId1, 11))) isE AVector(utxos(1), utxos(0), utxos(2))
-    selectWithoutGas(utxos, 20, AVector((tokenId1, 13))).leftValue
+    UtxoSelection(10).verify(1)
+    UtxoSelection(20).verify(1, 0)
+    UtxoSelection(20, (tokenId1, 1)).verify(1, 0)
+    UtxoSelection(20, (tokenId3, 5)).verify(1, 0, 2)
+    UtxoSelection(20, (tokenId1, 10)).verify(1, 0)
+    UtxoSelection(20, (tokenId1, 11)).verify(1, 0, 2)
+    UtxoSelection(20, (tokenId1, 13)).leftValue.startsWith(s"Not enough balance") is true
+
+    UtxoSelection(30).verify(1, 0)
+    UtxoSelection(30, (tokenId1, 10), (tokenId2, 15)).verify(1, 0)
+    UtxoSelection(40).verify(1, 0, 2)
+    UtxoSelection(40, (tokenId1, 10), (tokenId2, 20), (tokenId3, 10)).verify(1, 0, 2)
+    UtxoSelection(40, (tokenId1, 10), (tokenId2, 20), (tokenId3, 11)).leftValue
       .startsWith(s"Not enough balance") is true
 
-    selectWithoutGas(utxos, 30) isE AVector(utxos(1), utxos(0))
-    selectWithoutGas(utxos, 30, AVector((tokenId1, 10), (tokenId2, 15))) isE AVector(
-      utxos(1),
-      utxos(0)
-    )
-    selectWithoutGas(utxos, 40) isE AVector(utxos(1), utxos(0), utxos(2))
-    selectWithoutGas(
-      utxos,
-      40,
-      AVector((tokenId1, 10), (tokenId2, 20), (tokenId3, 10))
-    ) isE AVector(utxos(1), utxos(0), utxos(2))
-
-    selectWithoutGas(
-      utxos,
-      40,
-      AVector((tokenId1, 10), (tokenId2, 20), (tokenId3, 11))
-    ).leftValue.startsWith(s"Not enough balance") is true
-
-    selectWithoutGas(utxos, 70, AVector((tokenId1, 1))).leftValue
-      .startsWith(s"Not enough balance") is true
-
-    selectWithoutGas(
-      utxos,
-      29,
-      AVector((tokenId1, 10), (tokenId2, 15)),
-      dustAmount = 1
-    ) isE AVector(utxos(1), utxos(0))
-    selectWithoutGas(utxos, 29, AVector((tokenId1, 10), (tokenId2, 1)), dustAmount = 2) isE AVector(
-      utxos(1),
-      utxos(0),
-      utxos(2)
-    )
-    selectWithoutGas(
-      utxos,
-      30,
-      AVector((tokenId1, 10), (tokenId2, 20)),
-      dustAmount = 1
-    ) isE AVector(utxos(1), utxos(0))
-    selectWithoutGas(utxos, 59, AVector((tokenId1, 10)), dustAmount = 2).leftValue
+    UtxoSelection(70, (tokenId1, 1)).leftValue.startsWith(s"Not enough balance") is true
+    UtxoSelection(29, (tokenId1, 10), (tokenId2, 15)).withDust(1).verify(1, 0)
+    UtxoSelection(29, (tokenId1, 10), (tokenId2, 1)).withDust(2).verify(1, 0, 2)
+    UtxoSelection(29, (tokenId1, 10), (tokenId2, 20)).withDust(1).verify(1, 0)
+    UtxoSelection(59, (tokenId1, 10))
+      .withDust(2)
+      .leftValue
       .startsWith(s"Not enough balance") is true
   }
 
@@ -172,6 +147,30 @@ class UtxoUtilsSpec extends AlephiumSpec with LockupScriptGenerators {
       AVector.from(amounts.map { case (amount, tokens) =>
         buildOutput(defaultLockupScript, tokens, U256.unsafe(amount))
       })
+    }
+
+    case class UtxoSelection(amount: U256, tokens: (TokenId, U256)*)(implicit
+        utxos: AVector[AssetOutputInfo]
+    ) {
+      import UtxoUtils._
+      val utxosSorted = utxos.sorted(assetOrderByAlf)
+      lazy val result =
+        findUtxosWithoutGas(utxosSorted, amount, AVector.from(tokens), dustAmount).map {
+          case (_, selectedUtxos, _) => selectedUtxos
+        }
+      var dustAmount: U256 = U256.Zero
+
+      def verify(utxoIndexes: Int*): Assertion = {
+        val selectedUtxos = AVector.from(utxoIndexes).map(utxos(_))
+        result isE selectedUtxos
+      }
+
+      def withDust(newDustAmount: U256): UtxoSelection = {
+        dustAmount = newDustAmount
+        this
+      }
+
+      def leftValue = result.leftValue
     }
 
     def selectWithoutGas(
