374a1d3b9d6d4bb7e6ad26db94ced36af72f5030
Cheng Wang <wachmc@gmail.com>
2021-08-22 09:16:59 +0200

    Improve the fix

--- flow/src/test/scala/org/alephium/flow/network/DiscoveryServerSpec.scala
+++ flow/src/test/scala/org/alephium/flow/network/DiscoveryServerSpec.scala
@@ -124,14 +124,9 @@ class DiscoveryServerSpec
       }
     }
 
-    eventually {
-      servers.foreach { server =>
-        val probe = TestProbe()
-        server.tell(DiscoveryServer.GetNeighborPeers(None), probe.ref)
-
-        probe.expectMsgPF() { case DiscoveryServer.NeighborPeers(peers) =>
-          (peers.sumBy(peer => groups / peer.brokerNum) >= 5 * groups) is true
-        }
+    servers.foreach { server =>
+      withPeers(server) { peers =>
+        (peers.sumBy(peer => groups / peer.brokerNum) >= 5 * groups) is true
       }
     }
   }
@@ -159,26 +154,19 @@ class DiscoveryServerSpec
     server0 ! DiscoveryServer.SendCliqueInfo(cliqueInfo0)
     server1 ! DiscoveryServer.SendCliqueInfo(cliqueInfo1)
 
-    eventually {
-      val probe0 = TestProbe()
-      server0.tell(DiscoveryServer.GetNeighborPeers(None), probe0.ref)
-      val probe1 = TestProbe()
-      server1.tell(DiscoveryServer.GetNeighborPeers(None), probe1.ref)
-
-      probe0.expectMsgPF(probeTimeout) { case DiscoveryServer.NeighborPeers(peers) =>
-        peers.length is groups + 1 // self clique peers + server1
-        peers.filter(_.cliqueId equals cliqueInfo0.id).toSet is cliqueInfo0.interBrokers.get.toSet
-        peers.filterNot(_.cliqueId equals cliqueInfo0.id) is AVector(
-          cliqueInfo1.interBrokers.get.head
-        )
-      }
-      probe1.expectMsgPF(probeTimeout) { case DiscoveryServer.NeighborPeers(peers) =>
-        peers.length is groups + 1 // 3 self clique peers + server0
-        peers.filter(_.cliqueId equals cliqueInfo1.id).toSet is cliqueInfo1.interBrokers.get.toSet
-        peers.filterNot(_.cliqueId equals cliqueInfo1.id) is AVector(
-          cliqueInfo0.interBrokers.get.head
-        )
-      }
+    withPeers(server0) { peers =>
+      peers.length is groups + 1 // self clique peers + server1
+      peers.filter(_.cliqueId equals cliqueInfo0.id).toSet is cliqueInfo0.interBrokers.get.toSet
+      peers.filterNot(_.cliqueId equals cliqueInfo0.id) is AVector(
+        cliqueInfo1.interBrokers.get.head
+      )
+    }
+    withPeers(server1) { peers =>
+      peers.length is groups + 1 // 3 self clique peers + server0
+      peers.filter(_.cliqueId equals cliqueInfo1.id).toSet is cliqueInfo1.interBrokers.get.toSet
+      peers.filterNot(_.cliqueId equals cliqueInfo1.id) is AVector(
+        cliqueInfo0.interBrokers.get.head
+      )
     }
   }
 
@@ -187,33 +175,27 @@ class DiscoveryServerSpec
     server0 ! DiscoveryServer.SendCliqueInfo(cliqueInfo0)
     server1 ! DiscoveryServer.SendCliqueInfo(cliqueInfo1)
 
-    eventually {
-      val probe0 = TestProbe()
-      server0.tell(DiscoveryServer.GetNeighborPeers(None), probe0.ref)
-      probe0.expectMsgPF(probeTimeout) { case DiscoveryServer.NeighborPeers(peers) =>
-        peers.length is groups + 1 // self clique peers + server0
-        peers.map(_.address).contains(address1) is true
-      }
+    withPeers(server0) { peers =>
+      peers.length is groups + 1 // self clique peers + server1
+      peers.map(_.address).contains(address1) is true
+    }
+    withPeers(server1) { peers =>
+      peers.length is groups + 1 // self clique peers + server0
+      peers.map(_.address).contains(address0) is true
     }
 
     server0 ! MisbehaviorManager.PeerBanned(address1.getAddress)
 
-    eventually {
-      val probe0 = TestProbe()
-      server0.tell(DiscoveryServer.GetNeighborPeers(None), probe0.ref)
-      val probe1 = TestProbe()
-      server1.tell(DiscoveryServer.GetNeighborPeers(None), probe1.ref)
-
-      probe0.expectMsgPF(probeTimeout) { case DiscoveryServer.NeighborPeers(peers) =>
-        peers.length is groups // self clique peers
-      }
-      probe1.expectMsgPF(probeTimeout) { case DiscoveryServer.NeighborPeers(peers) =>
-        peers.length is groups + 1 // self clique peers + server0
-        peers.filter(_.cliqueId equals cliqueInfo1.id).toSet is cliqueInfo1.interBrokers.get.toSet
-        peers.filterNot(_.cliqueId equals cliqueInfo1.id) is AVector(
-          cliqueInfo0.interBrokers.get.head
-        )
-      }
+    withPeers(server0) { peers =>
+      // self clique peers might get removed as server0 and server1 have the same host address
+      (peers.length <= groups) is true
+    }
+    withPeers(server1) { peers =>
+      peers.length is groups + 1 // self clique peers + server0
+      peers.filter(_.cliqueId equals cliqueInfo1.id).toSet is cliqueInfo1.interBrokers.get.toSet
+      peers.filterNot(_.cliqueId equals cliqueInfo1.id) is AVector(
+        cliqueInfo0.interBrokers.get.head
+      )
     }
   }
 
@@ -237,13 +219,11 @@ class DiscoveryServerSpec
 
     override val groups = Gen.choose(2, 10).sample.get
 
-    val probeTimeout = Duration.ofSecondsUnsafe(5).asScala
-
     val port0               = generatePort()
     val (address0, config0) = createConfig(groups, port0, 2)
     val cliqueInfo0         = generateCliqueInfo(address0, config0)
     val port1               = generatePort()
-    val (address1, config1) = createConfig(groups, port1, 2, hostname = "127.0.0.2")
+    val (address1, config1) = createConfig(groups, port1, 2)
     val cliqueInfo1         = generateCliqueInfo(address1, config1)
     val misbehaviorManager0 = buildMisbehaviorManager(system)
     val misbehaviorManager1 = buildMisbehaviorManager(system)
@@ -263,6 +243,19 @@ class DiscoveryServerSpec
     val misbehaviorProbe = TestProbe()
     system.eventStream.subscribe(misbehaviorProbe.ref, classOf[MisbehaviorManager.InvalidMessage])
   }
+
+  def withPeers[T](
+      server0: ActorRefT[DiscoveryServer.Command]
+  )(check: AVector[BrokerInfo] => T): Unit = {
+    eventually {
+      val probe0 = TestProbe()
+      server0.tell(DiscoveryServer.GetNeighborPeers(None), probe0.ref)
+      probe0.expectMsgPF() { case DiscoveryServer.NeighborPeers(peers) =>
+        check(peers)
+      }
+      ()
+    }
+  }
 }
 
 object DiscoveryServerSpec {
@@ -273,10 +266,9 @@ object DiscoveryServerSpec {
       _peersPerGroup: Int,
       _scanFrequency: Duration = Duration.unsafe(200),
       _expireDuration: Duration = Duration.ofHoursUnsafe(1),
-      _peersTimeout: Duration = Duration.ofSecondsUnsafe(5),
-      hostname: String = "127.0.0.1"
+      _peersTimeout: Duration = Duration.ofSecondsUnsafe(5)
   ): (InetSocketAddress, DiscoveryConfig with BrokerConfig) = {
-    val publicAddress: InetSocketAddress = new InetSocketAddress(hostname, port)
+    val publicAddress: InetSocketAddress = new InetSocketAddress("127.0.0.1", port)
     val discoveryConfig = new DiscoveryConfig with BrokerConfig {
 
       val scanFrequency: Duration     = _scanFrequency
