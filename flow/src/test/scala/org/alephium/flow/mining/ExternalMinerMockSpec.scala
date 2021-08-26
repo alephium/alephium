dde8929aaf1bdac263be1316b3f6696d1fa3f19f
Cheng Wang <wachmc@gmail.com>
2021-08-26 18:12:45 +0200

    Refactor external miner tests

--- flow/src/test/scala/org/alephium/flow/mining/ExternalMinerMockSpec.scala
+++ flow/src/test/scala/org/alephium/flow/mining/ExternalMinerMockSpec.scala
@@ -18,8 +18,6 @@ package org.alephium.flow.mining
 
 import java.net.InetSocketAddress
 
-import scala.concurrent.ExecutionContext
-
 import akka.actor.ActorRef
 import akka.testkit.{TestActor, TestProbe}
 import org.scalatest.concurrent.Eventually
@@ -32,18 +30,35 @@ import org.alephium.util.{AlephiumActorSpec, AVector, Duration, SocketUtil}
 class ExternalMinerMockSpec extends AlephiumActorSpec with Eventually {
   override def actorSystemConfig = AlephiumActorSpec.infoConfig
 
-  it should "reconnect to multiple nodes" in new ExternalMinerMockFixture {
+  it should "fail to reconnect to multiple nodes immediately" in new ExternalMinerMockFixture {
+    override val configValues: Map[String, Any] = Map(
+      "alephium.network.backoff-base-delay" -> "10 s"
+    )
+
     minerApiControllers.foreach(_.underlyingActor.connections.size is 0)
     miner ! Miner.Start
     minerApiControllers.foreach(c => eventually(c.underlyingActor.connections.size is 1))
     minerApiControllers.foreach(c => system.stop(c.underlyingActor.connections(0).ref))
     minerApiControllers.foreach(c => eventually(c.underlyingActor.connections.size is 0))
+  }
+
+  it should "reconnect to multiple nodes" in new ExternalMinerMockFixture {
+    override val configValues: Map[String, Any] = Map(
+      "alephium.network.backoff-base-delay" -> "10 milli"
+    )
+
+    minerApiControllers.foreach(_.underlyingActor.connections.size is 0)
+    miner ! Miner.Start
     minerApiControllers.foreach(c => eventually(c.underlyingActor.connections.size is 1))
-    minerApiControllers.foreach(system.stop)
-    system.stop(miner)
+    minerApiControllers.foreach(c => system.stop(c.underlyingActor.connections(0).ref))
+    minerApiControllers.foreach(c => eventually(c.underlyingActor.connections.size is 1))
   }
 
   it should "shutdown if a minerApiController is stopped" in new ExternalMinerMockFixture {
+    override val configValues: Map[String, Any] = Map(
+      "alephium.network.backoff-base-delay" -> "10 milli"
+    )
+
     minerProbe watch miner
     minerApiControllers.foreach(c => c.underlyingActor.connections.size is 0)
     miner ! Miner.Start
@@ -55,6 +70,10 @@ class ExternalMinerMockSpec extends AlephiumActorSpec with Eventually {
   }
 
   it should "shutdown if a minerApiController connection is stopped" in new ExternalMinerMockFixture {
+    override val configValues: Map[String, Any] = Map(
+      "alephium.network.backoff-base-delay" -> "10 milli"
+    )
+
     minerProbe watch miner
     handlersProbes.head.viewHandler.setAutoPilot((sender: ActorRef, msg: Any) =>
       msg match {
@@ -71,28 +90,26 @@ class ExternalMinerMockSpec extends AlephiumActorSpec with Eventually {
   }
 
   trait ExternalMinerMockFixture extends SocketUtil with AlephiumConfigFixture {
-    implicit val executionContext: ExecutionContext = system.dispatcher
-    override val configValues: Map[String, Any] = Map(
-      "alephium.network.backoff-base-delay" -> "5 milli"
-    )
-
     lazy val controllersAddresses =
       (1 to groups0).map(_ => new InetSocketAddress(config.mining.apiInterface, generatePort()))
 
-    lazy val (handlers, handlersProbes) =
-      controllersAddresses.map(_ => TestUtils.createAllHandlersProbe).unzip
-
-    handlersProbes.foreach(
-      _.viewHandler.setAutoPilot((sender: ActorRef, msg: Any) =>
-        msg match {
-          case ViewHandler.Subscribe =>
-            sender ! ViewHandler.SubscribeResult(true)
-            TestActor.KeepRunning
-          case ViewHandler.Unsubscribe =>
-            TestActor.KeepRunning
-        }
-      )
-    )
+    lazy val (handlers, handlersProbes) = {
+      val handlers = controllersAddresses.map(_ => TestUtils.createAllHandlersProbe)
+      handlers
+        .map(_._2)
+        .foreach(
+          _.viewHandler.setAutoPilot((sender: ActorRef, msg: Any) =>
+            msg match {
+              case ViewHandler.Subscribe =>
+                sender ! ViewHandler.SubscribeResult(true)
+                TestActor.KeepRunning
+              case ViewHandler.Unsubscribe =>
+                TestActor.KeepRunning
+            }
+          )
+        )
+      handlers.unzip
+    }
 
     lazy val minerApiControllers =
       controllersAddresses.zip(handlers).map { case (address, allHandlers) =>
