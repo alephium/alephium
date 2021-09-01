23562aecb298b0eafe2640da569ca8c078b760ca
Cheng Wang <wachmc@gmail.com>
2021-09-01 23:45:28 +0200

    Fix upload/download monitoring panel

--- flow/src/main/scala/org/alephium/flow/network/broker/ConnectionHandler.scala
+++ flow/src/main/scala/org/alephium/flow/network/broker/ConnectionHandler.scala
@@ -111,7 +111,8 @@ trait ConnectionHandler[T] extends BaseActor with EventStream.Publisher {
 
   def bufferedCommunicating: Receive = reading orElse bufferedWriting orElse closed
 
-  private val downloadBytesTotalLabeled = downloadBytesTotal.labels(remoteAddress.toString)
+  private val downloadBytesTotalLabeled =
+    downloadBytesTotal.labels(remoteAddress.getAddress.getHostAddress)
   def reading: Receive = { case Tcp.Received(data) =>
     downloadBytesTotalLabeled.inc(data.length.toDouble)
     bufferInMessage(data)
@@ -241,7 +242,8 @@ trait ConnectionHandler[T] extends BaseActor with EventStream.Publisher {
     }
   }
 
-  private val uploadBytesTotalLabeled = uploadBytesTotal.labels(remoteAddress.toString)
+  private val uploadBytesTotalLabeled =
+    uploadBytesTotal.labels(remoteAddress.getAddress.getHostAddress)
   private def sendData(data: ByteString, ack: Long): Unit = {
     connection ! Tcp.Write(data, Ack(ack))
     uploadBytesTotalLabeled.inc(data.length.toDouble)
