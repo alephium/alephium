b75764daff8d4bedd56f3be2c574e74c7eadb816
lbqds <lbqds@outlook.com>
2021-10-21 18:47:48 +0800

    Add more tests for ConnectionHandler

--- flow/src/test/scala/org/alephium/flow/network/broker/ConnectionHandlerSpec.scala
+++ flow/src/test/scala/org/alephium/flow/network/broker/ConnectionHandlerSpec.scala
@@ -20,6 +20,7 @@ import java.net.InetSocketAddress
 
 import akka.io.Tcp
 import akka.testkit.{SocketUtil, TestActorRef, TestProbe}
+import akka.util.ByteString
 
 import org.alephium.flow.network.broker.ConnectionHandler.Ack
 import org.alephium.flow.setting.AlephiumConfigFixture
@@ -28,20 +29,31 @@ import org.alephium.protocol.message.{Header, Hello, Message, Ping, RequestId}
 import org.alephium.protocol.model.{BrokerInfo, CliqueId}
 import org.alephium.util.{AlephiumActorSpec, TimeStamp}
 
-class ConnectionHandlerSpec extends AlephiumActorSpec with AlephiumConfigFixture {
-  trait Fixture {
+class ConnectionHandlerSpec extends AlephiumActorSpec {
+  trait Fixture extends AlephiumConfigFixture {
     val remoteAddress = SocketUtil.temporaryServerAddress()
     val connection    = TestProbe()
     val brokerHandler = TestProbe()
 
-    val connectionHandler = TestActorRef[ConnectionHandler.CliqueConnectionHandler](
-      ConnectionHandler.clique(remoteAddress, connection.ref, brokerHandler.ref)
-    )
-    connection.expectMsgType[Tcp.Register]
-    connection.expectMsg(Tcp.ResumeReading)
+    lazy val connectionHandler = {
+      val handler = TestActorRef[ConnectionHandler.CliqueConnectionHandler](
+        ConnectionHandler.clique(remoteAddress, connection.ref, brokerHandler.ref)
+      )
+      connection.expectMsgType[Tcp.Register]
+      connection.expectMsg(Tcp.ResumeReading)
+      handler
+    }
 
-    val message      = Ping(RequestId.unsafe(1), TimeStamp.now())
-    val messageBytes = Message.serialize(message)
+    lazy val connectionHandlerActor = connectionHandler.underlyingActor
+    lazy val message                = Ping(RequestId.unsafe(1), TimeStamp.now())
+    lazy val messageBytes           = Message.serialize(message)
+
+    def switchToBufferedCommunicating() = {
+      connectionHandler ! ConnectionHandler.Send(messageBytes)
+      connection.expectMsg(Tcp.Write(messageBytes, Ack(1)))
+      connectionHandler ! Tcp.CommandFailed(Tcp.Write(messageBytes, Ack(1)))
+      connection.expectMsg(Tcp.ResumeWriting)
+    }
   }
 
   it should "publish misbehavior when receive invalid message" in new Fixture {
@@ -79,18 +91,18 @@ class ConnectionHandlerSpec extends AlephiumActorSpec with AlephiumConfigFixture
   it should "buffer data when writing is failing" in new Fixture {
     connectionHandler ! Tcp.CommandFailed(Tcp.Write(messageBytes, Ack(-1)))
     connection.expectMsg(Tcp.ResumeWriting)
-    connectionHandler.underlyingActor.outMessageBuffer.size is 1
-    connectionHandler.underlyingActor.outMessageCount is 0
+    connectionHandlerActor.outMessageBuffer.size is 1
+    connectionHandlerActor.outMessageCount is 0
 
     connectionHandler ! Tcp.CommandFailed(Tcp.Write(messageBytes, Ack(0)))
     connection.expectMsg(Tcp.ResumeWriting)
-    connectionHandler.underlyingActor.outMessageBuffer.size is 2
-    connectionHandler.underlyingActor.outMessageCount is 0
+    connectionHandlerActor.outMessageBuffer.size is 2
+    connectionHandlerActor.outMessageCount is 0
 
     connectionHandler ! ConnectionHandler.Send(messageBytes)
     connection.expectNoMessage()
-    connectionHandler.underlyingActor.outMessageBuffer.size is 3
-    connectionHandler.underlyingActor.outMessageCount is 1
+    connectionHandlerActor.outMessageBuffer.size is 3
+    connectionHandlerActor.outMessageCount is 1
 
     connectionHandler ! Tcp.WritingResumed
     connection.expectMsg(Tcp.Write(messageBytes, Ack(-1)))
@@ -102,5 +114,63 @@ class ConnectionHandlerSpec extends AlephiumActorSpec with AlephiumConfigFixture
     connection.expectMsg(Tcp.Close)
     connectionHandler ! Tcp.Closed
     expectTerminated(connectionHandler)
+  }
+
+  it should "close connection when write buffer overrun" in new Fixture {
+    override val configValues = Map(
+      "alephium.network.connection-buffer-capacity-in-byte" -> 100
+    )
+
+    watch(connectionHandler)
+    switchToBufferedCommunicating()
+    val data = Array.fill[Byte](100)(0x01)
+    connectionHandler ! ConnectionHandler.Send(ByteString.fromArrayUnsafe(data))
+    expectTerminated(connectionHandler)
+  }
+
+  it should "write all buffered data to remote when half closing" in new Fixture {
+    switchToBufferedCommunicating()
+    val datas = (2 until 6).map { idx =>
+      val data = ByteString.fromArrayUnsafe(Array.fill[Byte](100)(idx.toByte))
+      connectionHandler ! ConnectionHandler.Send(data)
+      connectionHandlerActor.outMessageBuffer(idx.toLong) is data
+      data
+    }
+
+    connectionHandler ! Tcp.PeerClosed
+    connection.expectMsg(Tcp.Write(messageBytes, Ack(1)))
+    datas.zipWithIndex.foreach { case (data, idx) =>
+      connection.expectMsg(Tcp.Write(data, Ack(idx.toLong + 2)))
+    }
+    connectionHandler ! Ack(1)
+    connectionHandlerActor.outMessageBuffer.contains(1) is false
+
+    connectionHandler ! Tcp.CommandFailed(Tcp.Write(datas.head, Ack(2)))
+    connection.expectMsg(Tcp.ResumeWriting)
+    connectionHandler ! Tcp.WritingResumed
+    datas.zipWithIndex.foreach { case (data, idx) =>
+      val id = idx.toLong + 2
+      connection.expectMsg(Tcp.Write(data, Ack(id)))
+      connectionHandler ! Ack(id)
+    }
+    connectionHandlerActor.outMessageBuffer.isEmpty is true
+    connection.expectMsg(Tcp.Close)
+  }
+
+  it should "switch to `communicating` when all buffered data write succeed" in new Fixture {
+    switchToBufferedCommunicating()
+    connectionHandlerActor.outMessageBuffer.size is 1
+    connectionHandler ! ConnectionHandler.Send(messageBytes)
+    connectionHandlerActor.outMessageBuffer.size is 2
+    connectionHandler ! Tcp.WritingResumed
+    connection.expectMsg(Tcp.Write(messageBytes, Ack(1)))
+    connectionHandler ! Ack(1)
+    connection.expectMsg(Tcp.Write(messageBytes, Ack(2)))
+    connectionHandler ! Ack(2)
+    connectionHandlerActor.outMessageBuffer.size is 0
+
+    connectionHandler ! ConnectionHandler.Send(messageBytes)
+    connection.expectMsg(Tcp.Write(messageBytes, Ack(3)))
+    connectionHandlerActor.outMessageBuffer.size is 0
   }
 }
