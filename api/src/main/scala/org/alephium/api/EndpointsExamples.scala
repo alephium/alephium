690e46e61158e0ea6edefaafe4ce1f38f1453024
Hongchao Liu <lbs.lhc@gmail.com>
2022-05-17 09:59:53 +0200

    Fetch events locally when possible

--- api/src/main/scala/org/alephium/api/EndpointsExamples.scala
+++ api/src/main/scala/org/alephium/api/EndpointsExamples.scala
@@ -645,8 +645,8 @@ trait EndpointsExamples extends ErrorExamples {
   implicit val verifySignatureExamples: List[Example[VerifySignature]] =
     simpleExample(VerifySignature(Hex.unsafe(hexString), signature, publicKey))
 
-  implicit val eventsExamples: List[Example[Events]] =
-    simpleExample(Events(0, 1, events = AVector(event), 2))
+  implicit val eventsExamples: List[Example[Option[Events]]] =
+    simpleExample(Some(Events(0, 1, events = AVector(event), 2)))
 
   implicit val eventsVectorExamples: List[Example[AVector[Events]]] =
     simpleExample(AVector(Events(0, 1, events = AVector(event), 3)))
