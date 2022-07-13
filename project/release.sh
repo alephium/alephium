70ad5ba0d02930c9610b0057ba47d6b9565e276c
Cheng Wang <wachmc@gmail.com>
2022-07-13 13:07:31 +0200

    Fix release.sh

--- project/release.sh
+++ project/release.sh
@@ -4,9 +4,10 @@ set -e
 
 openapi_json="api/src/main/resources/openapi.json"
 new_version=$1
+new_version_without_postfix=$(echo $new_version | cut -d- -f1)
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 
-if ! [[ "${new_version}" =~ [0-9]+.[0-9]+.[0-9].* ]]; then
+if ! [[ "${new_version}" =~ [0-9]+.[0-9]+.[0-9](-.*)?$ ]]; then
   echo "Invalid version ${new_version}"
   exit 1
 fi
@@ -22,15 +23,15 @@ if [[ ! -z $(git status --porcelain) ]]; then
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  sed -E -i 's/"version": "[0-9\.]+"/"version": "'"${new_version}"'"/g' $openapi_json
+  sed -E -i 's/"version": "[0-9\.]+"/"version": "'"${new_version_without_postfix}"'"/g' $openapi_json
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -E -i '' 's/"version": "[0-9\.]+"/"version": "'"${new_version}"'"/g' $openapi_json
+  sed -E -i '' 's/"version": "[0-9\.]+"/"version": "'"${new_version_without_postfix}"'"/g' $openapi_json
 else
   echo "Unsupported system $OSTYPE"
   exit 1
 fi
 
-git add -A && git commit -m "${new_version}"
+git add -A && git commit -m "${new_version}" --allow-empty
 git tag v$new_version
 git push origin v$new_version
 git push origin $branch_name:$branch_name
