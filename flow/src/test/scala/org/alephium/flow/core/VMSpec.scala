f23209d98bd2637a775b934249bc1b5d84cd6066
Cheng Wang <wachmc@gmail.com>
2023-02-09 14:16:59 +0100

    Fix unit tests

--- flow/src/test/scala/org/alephium/flow/core/VMSpec.scala
+++ flow/src/test/scala/org/alephium/flow/core/VMSpec.scala
@@ -626,12 +626,15 @@ class VMSpec extends AlephiumSpec {
          |""".stripMargin
     callTxScript(mint)
 
+    val timestamp0 = TimeStamp.now().plusHoursUnsafe(1)
+    val timestamp1 = TimeStamp.now().plusHoursUnsafe(2)
+    val timestamp2 = TimeStamp.now().plusHoursUnsafe(3)
     val lock =
       s"""
          |TxScript Main {
-         |  let timestamp0 = 1000
-         |  let timestamp1 = 2000
-         |  let timestamp2 = 3000
+         |  let timestamp0 = ${timestamp0.millis}
+         |  let timestamp1 = ${timestamp1.millis}
+         |  let timestamp2 = ${timestamp2.millis}
          |
          |  lockApprovedAssets!{ @$genesisAddress -> ALPH: 0.01 alph }(@$genesisAddress, timestamp0)
          |
@@ -648,27 +651,28 @@ class VMSpec extends AlephiumSpec {
          |""".stripMargin
 
     val tx = callTxScript(lock).nonCoinbase(0)
-    tx.generatedOutputs(0) is AssetOutput(
-      ALPH.cent(1),
-      genesisAddress.lockupScript,
-      TimeStamp.unsafe(1000),
-      AVector.empty,
-      ByteString.empty
-    )
-    tx.generatedOutputs(1) is AssetOutput(
-      ALPH.cent(2),
-      genesisAddress.lockupScript,
-      TimeStamp.unsafe(2000),
-      AVector(tokenId0 -> ALPH.cent(3)),
-      ByteString.empty
-    )
-    tx.generatedOutputs(2) is AssetOutput(
-      ALPH.cent(4),
-      genesisAddress.lockupScript,
-      TimeStamp.unsafe(3000),
-      AVector(tokenId0 -> ALPH.cent(5), tokenId1 -> ALPH.cent(6)),
-      ByteString.empty
-    )
+
+    def checkOutput(
+        index: Int,
+        timestamp: TimeStamp,
+        alphAmount: U256,
+        tokens: (TokenId, U256)*
+    ) = {
+      tx.generatedOutputs(index) is AssetOutput(
+        alphAmount,
+        genesisAddress.lockupScript,
+        timestamp,
+        AVector.from(tokens),
+        ByteString.empty
+      )
+    }
+
+    checkOutput(0, timestamp0, ALPH.cent(1))
+    checkOutput(1, timestamp1, dustUtxoAmount, tokenId0 -> ALPH.cent(3))
+    checkOutput(2, timestamp1, ALPH.cent(2) - dustUtxoAmount)
+    checkOutput(3, timestamp2, dustUtxoAmount, tokenId0 -> ALPH.cent(5))
+    checkOutput(4, timestamp2, dustUtxoAmount, tokenId1 -> ALPH.cent(6))
+    checkOutput(5, timestamp2, ALPH.cent(4) - (dustUtxoAmount * 2))
   }
 
   it should "not use up contract assets" in new ContractFixture {
@@ -1818,7 +1822,7 @@ class VMSpec extends AlephiumSpec {
         .length is numContractOutput
     }
 
-    checkSwapBalance(minimalAlphInContract, 0, 4, 3)
+    checkSwapBalance(minimalAlphInContract, 0, 5, 3)
 
     callTxScript(s"""
                     |TxScript Main {
@@ -1830,7 +1834,7 @@ class VMSpec extends AlephiumSpec {
                     |
                     |${AMMContract.swapContract}
                     |""".stripMargin)
-    checkSwapBalance(minimalAlphInContract + 10, 100, 5 /* 1 more coinbase output */, 3)
+    checkSwapBalance(minimalAlphInContract + 10, 100, 6 /* 1 more coinbase output */, 3)
 
     callTxScript(s"""
                     |TxScript Main {
@@ -1840,7 +1844,7 @@ class VMSpec extends AlephiumSpec {
                     |
                     |${AMMContract.swapContract}
                     |""".stripMargin)
-    checkSwapBalance(minimalAlphInContract + 20, 50, 6 /* 1 more coinbase output */, 3)
+    checkSwapBalance(minimalAlphInContract + 20, 50, 7 /* 1 more coinbase output */, 3)
 
     callTxScript(
       s"""
@@ -1852,7 +1856,7 @@ class VMSpec extends AlephiumSpec {
          |${AMMContract.swapContract}
          |""".stripMargin
     )
-    checkSwapBalance(minimalAlphInContract + 10, 100, 7 /* 1 more coinbase output */, 3)
+    checkSwapBalance(minimalAlphInContract + 10, 100, 8 /* 1 more coinbase output */, 3)
   }
 
   trait TxExecutionOrderFixture extends ContractFixture {
