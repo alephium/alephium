c0c6db38fc309baf882eb7b1aa9af5b55841e6d5
Cheng Wang <wachmc@gmail.com>
2023-03-26 14:33:48 +0200

    Fix docker entrypoint script

--- docker/release/entrypoint.sh
+++ docker/release/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-echo "Starting java -jar /alephium.jar ${JAVA_NET_OPTS} ${JAVA_MEM_OPTS} ${JAVA_GC_OPTS} ${JAVA_EXTRA_OPTS} $@"
-exec java -jar /alephium.jar ${JAVA_NET_OPTS} ${JAVA_MEM_OPTS} ${JAVA_GC_OPTS} ${JAVA_EXTRA_OPTS} $@
+echo "Starting java ${JAVA_NET_OPTS} ${JAVA_MEM_OPTS} ${JAVA_GC_OPTS} ${JAVA_EXTRA_OPTS} -jar /alephium.jar $@"
+exec java ${JAVA_NET_OPTS} ${JAVA_MEM_OPTS} ${JAVA_GC_OPTS} ${JAVA_EXTRA_OPTS} -jar /alephium.jar $@
