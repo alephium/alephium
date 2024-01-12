e1ac049efc129e32803f85f195f330d682c3b485
h0ngcha0 <lbs.lhc@gmail.com>
2024-01-12 20:38:22 +0100

    Update entrypoint for Dockerfile.tools.release

--- docker/release/entrypoint-tools.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Starting java ${JAVA_NET_OPTS} ${JAVA_MEM_OPTS} ${JAVA_GC_OPTS} ${JAVA_EXTRA_OPTS} -cp /alephium-tools.jar $@"
-exec java ${JAVA_NET_OPTS} ${JAVA_MEM_OPTS} ${JAVA_GC_OPTS} ${JAVA_EXTRA_OPTS} -cp /alephium.jar $@
