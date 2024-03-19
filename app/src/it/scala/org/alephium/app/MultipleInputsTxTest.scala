bcc903d30df964fe31dc62af6fb89c80d3dff4be
Thomas Droxler <thomas.droxler@gmail.com>
2024-03-19 15:47:54 +0100

    Add an it test where one address pay gas for everyone

--- app/src/it/scala/org/alephium/app/MultipleInputsTxTest.scala
+++ app/src/it/scala/org/alephium/app/MultipleInputsTxTest.scala
@@ -21,6 +21,7 @@ import akka.util.ByteString
 import org.alephium.api.model._
 import org.alephium.protocol._
 import org.alephium.protocol.model.Address
+import org.alephium.protocol.vm.GasBox
 import org.alephium.util._
 
 class MultipleInputsTxTest extends AlephiumActorSpec {
@@ -149,5 +150,101 @@ class MultipleInputsTxTest extends AlephiumActorSpec {
     clique.stop()
   }
 
+  it should "let only one address pay gas" in new CliqueFixture {
+    val clique = bootClique(nbOfNodes = 1)
+    clique.start()
+
+    def currentUTXOs(addr: String) = {
+      request[UTXOs](getUTXOs(addr), clique.masterRestPort)
+    }
+
+    val addressGroupIndex = Address.fromBase58(address).get.groupIndex
+
+    val (address2, publicKey2, privateKey2) = generateAccount(addressGroupIndex)
+    val (address3, publicKey3, privateKey3) = generateAccount(addressGroupIndex)
+    val (address4, publicKey4, privateKey4) = generateAccount(addressGroupIndex)
+    val (destAddress, _, _)                 = generateAccount(addressGroupIndex)
+
+    val tx =
+      transfer(publicKey, address2, transferAmount, privateKey, clique.masterRestPort)
+    val tx2 =
+      transfer(publicKey, address2, transferAmount, privateKey, clique.masterRestPort)
+    val tx3 =
+      transfer(publicKey, address3, transferAmount, privateKey, clique.masterRestPort)
+    val tx4 =
+      transfer(publicKey, address4, transferAmount, privateKey, clique.masterRestPort)
+
+    clique.startMining()
+    confirmTx(tx, clique.masterRestPort)
+    confirmTx(tx2, clique.masterRestPort)
+    confirmTx(tx3, clique.masterRestPort)
+    confirmTx(tx4, clique.masterRestPort)
+
+    val utxos = currentUTXOs(address).utxos
+
+    val amount = utxos.map(_.amount.value).fold(U256.Zero)(_ addUnsafe _).divUnsafe(U256.Two)
+    // Force Two utxos
+    val amount2 = transferAmount.mulUnsafe(U256.Two) - transferAmount.divUnsafe(U256.Two)
+
+    val utxos3  = currentUTXOs(address3).utxos
+    val amount3 = utxos3.map(_.amount.value).fold(U256.Zero)(_ addUnsafe _).divUnsafe(U256.Two)
+
+    val utxos4      = currentUTXOs(address4).utxos
+    val amountInit4 = utxos4.map(_.amount.value).fold(U256.Zero)(_ addUnsafe _)
+    val amount4     = amountInit4.divUnsafe(U256.Two)
+
+    def destinations(
+        amount: U256,
+        lockTime: Option[TimeStamp] = None,
+        message: Option[ByteString] = None
+    ) = AVector(
+      Destination(
+        Address.asset(destAddress).get,
+        Amount(amount),
+        lockTime = lockTime,
+        message = message
+      )
+    )
+
+    val inputs: AVector[BuildMultiAddressesTransaction.Source] = AVector(
+      BuildMultiAddressesTransaction
+        .Source(pubKey(publicKey).bytes, destinations(amount), gasAmount = None),
+      BuildMultiAddressesTransaction
+        .Source(pubKey(publicKey2).bytes, destinations(amount2), gasAmount = Some(GasBox.zero)),
+      BuildMultiAddressesTransaction
+        .Source(
+          pubKey(publicKey3).bytes,
+          destinations(amount3, lockTime = Some(TimeStamp.now())),
+          gasAmount = Some(GasBox.zero)
+        ),
+      BuildMultiAddressesTransaction.Source(
+        pubKey(publicKey4).bytes,
+        destinations(amount4, message = Some(ByteString.empty)),
+        gasAmount = Some(GasBox.zero)
+      )
+    )
+
+    val genericTx = transferGeneric(
+      inputs,
+      AVector(privateKey, privateKey2, privateKey3, privateKey4),
+      clique.masterRestPort
+    )
+
+    confirmTx(genericTx, clique.masterRestPort)
+
+    clique.stopMining()
+
+    // not only 1 utoxs as there are input with locktime and message
+    currentUTXOs(destAddress).utxos.length is 3
+
+    val amount4After =
+      currentUTXOs(address4).utxos.map(_.amount.value).fold(U256.Zero)(_ addUnsafe _)
+
+    amount4After is amountInit4.subUnsafe(amount4)
+
+    clique.stop()
+  }
+
   def pubKey(str: String): PublicKey = PublicKey.unsafe(Hex.unsafe(str))
+
 }
