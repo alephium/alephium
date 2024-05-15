f6d409ecac05b9a73b3b8c61806451f504e061c0
h0ngcha0 <lbs.lhc@gmail.com>
2024-05-15 09:07:37 +0200

    Test loading NodeIndexesConfig

--- flow/src/test/scala/org/alephium/flow/setting/AlephiumConfigSpec.scala
+++ flow/src/test/scala/org/alephium/flow/setting/AlephiumConfigSpec.scala
@@ -41,7 +41,7 @@ import org.alephium.protocol.model.{
   HardFork,
   NetworkId
 }
-import org.alephium.protocol.vm.LogConfig
+import org.alephium.protocol.vm.{LogConfig, NodeIndexesConfig}
 import org.alephium.util.{AlephiumSpec, AVector, Duration, Env, Files, Hex, TimeStamp}
 
 class AlephiumConfigSpec extends AlephiumSpec {
@@ -297,6 +297,81 @@ class AlephiumConfigSpec extends AlephiumSpec {
       ConfigFactory
         .parseString(configs)
         .as[LogConfig]("event-log")(ValueReader[LogConfig]) is logConfig
+    }
+  }
+
+  it should "load NodeIndexesConfig" in {
+    val configs =
+      s"""
+         |{
+         |  indexes {
+         |    tx-output-ref-index = true
+         |    subcontract-index = true
+         |  }
+         |}
+         |""".stripMargin
+
+    ConfigFactory
+      .parseString(configs)
+      .as[NodeIndexesConfig]("indexes")(ValueReader[NodeIndexesConfig]) is NodeIndexesConfig(
+      txOutputRefIndex = true,
+      subcontractIndex = true
+    )
+  }
+
+  it should "load NodeSetting" in {
+    {
+      info("Without indexes config")
+      val configs =
+        s"""
+           |{
+           |  node {
+           |    db-sync-write = false
+           |    event-log {
+           |      enabled = true
+           |      index-by-tx-id = true
+           |      index-by-block-hash = true
+           |    }
+           |  }
+           |}
+           |""".stripMargin
+
+      ConfigFactory
+        .parseString(configs)
+        .as[NodeSetting]("node")(ValueReader[NodeSetting]) is NodeSetting(
+        dbSyncWrite = false,
+        LogConfig.allEnabled(),
+        None
+      )
+    }
+
+    {
+      info("With indexes config")
+      val configs =
+        s"""
+           |{
+           |  node {
+           |    db-sync-write = false
+           |    event-log {
+           |      enabled = true
+           |      index-by-tx-id = true
+           |      index-by-block-hash = true
+           |    }
+           |    indexes {
+           |      tx-output-ref-index = true
+           |      subcontract-index = false
+           |    }
+           |  }
+           |}
+           |""".stripMargin
+
+      ConfigFactory
+        .parseString(configs)
+        .as[NodeSetting]("node")(ValueReader[NodeSetting]) is NodeSetting(
+        dbSyncWrite = false,
+        LogConfig.allEnabled(),
+        Some(NodeIndexesConfig(txOutputRefIndex = true, subcontractIndex = false))
+      )
     }
   }
 
