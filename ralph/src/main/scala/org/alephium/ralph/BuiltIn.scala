1f7cea9c9401178947075806db9df263993765c1
h0ngcha0 <lbs.lhc@gmail.com>
2024-09-02 18:02:57 +0200

    Improve callerAddress doc

--- ralph/src/main/scala/org/alephium/ralph/BuiltIn.scala
+++ ralph/src/main/scala/org/alephium/ralph/BuiltIn.scala
@@ -1554,14 +1554,20 @@ object BuiltIn {
     )
 
   val callerAddress: SimpleBuiltIn[StatefulContext] =
-    SimpleBuiltIn.contractSimple(
+    SimpleBuiltIn.contract(
       "callerAddress",
       Seq.empty,
       Seq(Type.Address),
       CallerAddress,
       argsName = Seq(),
-      retComment =
-        "the address of the caller. When used in a TxScript, it returns the unique input address if the input addresses are the same, otherwise it fails"
+      retComment = "the address of the caller",
+      doc =
+        s"""
+           |1. When used in a TxScript, returns the transaction caller, which is the first input address when all input addresses are the same.
+           |   If not all input addresses are the same, `callAddress!()` function fails.
+           |2. When used in a contract function called directly from TxScript, returns the transaction caller as explained in 1)
+           |3. When used in a contract function called from another contract, returns the address of the calling contract.
+      """.stripMargin
     )
 
   val isCalledFromTxScript: SimpleBuiltIn[StatefulContext] =
