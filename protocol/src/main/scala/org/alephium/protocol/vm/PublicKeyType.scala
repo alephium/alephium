1c3127b9c2f46bd1a828e3f05ff2c30f66101c08
lbqds <lbqds@outlook.com>
2024-11-27 16:44:44 +0800

    Introduce PublicKeyType

---
+++ protocol/src/main/scala/org/alephium/protocol/vm/PublicKeyType.scala
@@ -0,0 +1,43 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.protocol.vm
+
+import akka.util.ByteString
+
+import org.alephium.crypto.SecP256K1PublicKey
+import org.alephium.serde._
+
+sealed trait PublicKeyType
+
+object PublicKeyType {
+  implicit val serde: Serde[PublicKeyType] = new Serde[PublicKeyType] {
+    override def serialize(input: PublicKeyType): ByteString = input match {
+      case SecP256K1(publicKey) =>
+        ByteString(0) ++ serdeImpl[SecP256K1PublicKey].serialize(publicKey)
+    }
+
+    override def _deserialize(input: ByteString): SerdeResult[Staging[PublicKeyType]] = {
+      byteSerde._deserialize(input).flatMap {
+        case Staging(0, rest) =>
+          serdeImpl[SecP256K1PublicKey]._deserialize(rest).map(_.mapValue(SecP256K1.apply))
+        case Staging(n, _) => Left(SerdeError.wrongFormat(s"Invalid public key type $n"))
+      }
+    }
+  }
+
+  final case class SecP256K1(publicKey: SecP256K1PublicKey) extends PublicKeyType
+}
---
+++ protocol/src/test/scala/org/alephium/protocol/vm/PublicKeyTypeSpec.scala
@@ -0,0 +1,33 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.protocol.vm
+
+import akka.util.ByteString
+
+import org.alephium.crypto.SecP256K1PublicKey
+import org.alephium.serde.{deserialize, serialize}
+import org.alephium.util.AlephiumSpec
+
+class PublicKeyTypeSpec extends AlephiumSpec {
+  it should "serde correctly" in {
+    val publicKey = PublicKeyType.SecP256K1(SecP256K1PublicKey.generate)
+    val bytes     = ByteString(0) ++ publicKey.publicKey.bytes
+    serialize[PublicKeyType](publicKey) is bytes
+    deserialize[PublicKeyType](bytes) isE publicKey
+    deserialize[PublicKeyType](ByteString(1)).leftValue.getMessage is "Invalid public key type 1"
+  }
+}
