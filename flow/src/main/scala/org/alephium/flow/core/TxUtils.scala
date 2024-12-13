f0aeafc37d88f7a1c283545f25b92e89bb72e8f0
Cheng Wang <wachmc@gmail.com>
2024-12-13 23:08:40 +0100

    Remove getTransaction from BlockChain

--- flow/src/main/scala/org/alephium/flow/core/TxUtils.scala
+++ flow/src/main/scala/org/alephium/flow/core/TxUtils.scala
@@ -1390,19 +1390,6 @@ trait TxUtils { Self: FlowUtils =>
     }
   }
 
-  def getTransaction(
-      txId: TransactionId,
-      block: Block
-  ): Either[String, Option[Transaction]] = {
-    val chainIndex = ChainIndex.from(block.hash)
-    if (brokerConfig.contains(chainIndex.from)) {
-      val chain = Self.blockFlow.getBlockChain(chainIndex)
-      chain.getTransaction(txId, block).left.map(_.toString)
-    } else {
-      Right(None)
-    }
-  }
-
   def searchTransaction(
       txId: TransactionId,
       chainIndexes: AVector[ChainIndex]
