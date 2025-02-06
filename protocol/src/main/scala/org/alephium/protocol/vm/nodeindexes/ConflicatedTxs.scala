a901cfbbba88e71d6202ee6b6738896d575610e4
Cheng Wang <wachmc@gmail.com>
2025-02-06 14:21:20 +0100

    Add storage for conflicted txs

---
+++ protocol/src/main/scala/org/alephium/protocol/vm/nodeindexes/ConflicatedTxs.scala
@@ -0,0 +1,67 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.protocol.vm.nodeindexes
+
+import org.alephium.io.{CachedKVStorage, IOResult, KeyValueStorage}
+import org.alephium.protocol.model.{BlockHash, TransactionId}
+import org.alephium.serde._
+import org.alephium.util.AVector
+
+final case class ConflictedTxsPerBlock(block: BlockHash, txs: AVector[TransactionId])
+object ConflictedTxsPerBlock {
+  implicit val serde: Serde[ConflictedTxsPerBlock] =
+    Serde.forProduct2(ConflictedTxsPerBlock.apply, t => (t.block, t.txs))
+}
+final case class ConflictedTxsSource(
+    intraBlock: BlockHash,
+    txs: AVector[TransactionId]
+)
+object ConflictedTxsSource {
+  implicit val serde: Serde[ConflictedTxsSource] =
+    Serde.forProduct2(ConflictedTxsSource.apply, t => (t.intraBlock, t.txs))
+}
+
+// Storage for tracking conflicted transactions in blocks
+// A transaction is conflicted when its inputs are already spent by another transaction
+final case class ConflictedTxsStorage(
+    // Maps an intra-block (checkpoint block) hash to a list of inter-group blocks containing conflicted transactions
+    conflictedTxsPerIntraBlock: KeyValueStorage[BlockHash, AVector[ConflictedTxsPerBlock]],
+    // Reverse index of the index above, mapping inter-group block hash to the checkpoint block hash and the conflicted transactions
+    conflictedTxsReversedIndex: KeyValueStorage[BlockHash, AVector[ConflictedTxsSource]]
+) {
+  def cache(): CachedConflictedTxsStorage = {
+    CachedConflictedTxsStorage(
+      CachedKVStorage.from(conflictedTxsPerIntraBlock),
+      CachedKVStorage.from(conflictedTxsReversedIndex)
+    )
+  }
+}
+
+final case class CachedConflictedTxsStorage(
+    conflictedTxsPerIntraBlock: CachedKVStorage[BlockHash, AVector[ConflictedTxsPerBlock]],
+    conflictedTxsReversedIndex: CachedKVStorage[BlockHash, AVector[ConflictedTxsSource]]
+) {
+  def persist(): IOResult[ConflictedTxsStorage] = {
+    for {
+      conflictedTxsPerIntraBlockStorage <- conflictedTxsPerIntraBlock.persist()
+      conflictedTxsReversedIndexStorage <- conflictedTxsReversedIndex.persist()
+    } yield ConflictedTxsStorage(
+      conflictedTxsPerIntraBlockStorage,
+      conflictedTxsReversedIndexStorage
+    )
+  }
+}
