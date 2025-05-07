b07f8e08057c2ba946d7fe702d9e251026d3b60e
Cheng Wang <wachmc@gmail.com>
2025-05-07 11:03:54 +0200

    Introduce lazy template broadcast

--- flow/src/main/scala/org/alephium/flow/mining/CpuMiner.scala
+++ flow/src/main/scala/org/alephium/flow/mining/CpuMiner.scala
@@ -67,7 +67,7 @@ class CpuMiner(val allHandlers: AllHandlers)(implicit
       if (miningStarted) {
         updateAndStartTasks(templates)
       }
-    case ViewHandler.NewTemplate(template) =>
+    case ViewHandler.NewTemplate(template, _) =>
       if (miningStarted) {
         updateAndStartTask(template)
       }
